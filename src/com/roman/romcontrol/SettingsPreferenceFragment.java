9d487062925f50ddc7b6d08ec3a21885f3f8fadd
Roman Birg <romanbirg@gmail.com>
2012-02-06 18:29:56 -0600

    add mContext for easier use

--- src/com/roman/romcontrol/SettingsPreferenceFragment.java
+++ src/com/roman/romcontrol/SettingsPreferenceFragment.java
@@ -20,6 +20,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -35,18 +36,15 @@ import android.widget.Button;
 public class SettingsPreferenceFragment extends PreferenceFragment implements DialogCreatable {
 
     private static final String TAG = "SettingsPreferenceFragment";
+    protected Context mContext;
 
     private SettingsDialogFragment mDialogFragment;
     protected boolean mTablet;
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         mTablet = Settings.System.getInt(getContentResolver(), Settings.System.IS_TABLET, 0) == 1;
+        mContext = getActivity().getApplicationContext();
         super.onCreate(savedInstanceState);
     }
 

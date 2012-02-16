a45477998c3da28cb27f36de79b4b34e760edeeb
Roman Birg <romanbirg@gmail.com>
2012-02-16 11:26:48 -0500

    add dBm text options

---
+++ src/com/roman/romcontrol/fragments/StatusBarSignal.java
@@ -0,0 +1,70 @@
+
+package com.roman.romcontrol.fragments;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.roman.romcontrol.R;
+import com.roman.romcontrol.SettingsPreferenceFragment;
+
+public class StatusBarSignal extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    ListPreference mDbmStyletyle;
+    ColorPickerPreference mColorPicker;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.prefs_statusbar_signal);
+
+        mDbmStyletyle = (ListPreference) findPreference("signal_style");
+        mDbmStyletyle.setOnPreferenceChangeListener(this);
+        mDbmStyletyle.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.STATUSBAR_SIGNAL_TEXT,
+                0)));
+
+        mColorPicker = (ColorPickerPreference) findPreference("signal_color");
+        mColorPicker.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            Preference preference) {
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mDbmStyletyle) {
+
+            int val = Integer.parseInt((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_SIGNAL_TEXT, val);
+            return true;
+
+        } else if (preference == mColorPicker) {
+            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                    .valueOf(newValue)));
+            preference.setSummary(hex);
+
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_SIGNAL_TEXT_COLOR, intHex);
+
+            return true;
+        }
+        return false;
+    }
+}

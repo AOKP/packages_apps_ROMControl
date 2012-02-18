b8bc300e675fb5e76e9478c6c49fd3174fe8a07b
Roman Birg <romanbirg@gmail.com>
2012-02-18 14:27:44 -0600

    add aokp about page

---
+++ src/com/roman/romcontrol/fragments/About.java
@@ -0,0 +1,49 @@
+
+package com.roman.romcontrol.fragments;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+
+import com.roman.romcontrol.R;
+import com.roman.romcontrol.SettingsPreferenceFragment;
+
+public class About extends SettingsPreferenceFragment {
+
+    public static final String TAG = "About";
+
+    Preference mSiteUrl;
+    Preference mSourceUrl;
+    Preference mIrcUrl;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.prefs_about);
+        mSiteUrl = findPreference("aokp_website");
+        mSourceUrl = findPreference("aokp_source");
+        mIrcUrl = findPreference("aokp_irc");
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mSiteUrl) {
+            launchUrl("http://aokp.co/");
+        } else if (preference == mSourceUrl) {
+            launchUrl("http://github.com/aokp");
+        } else if (preference == mIrcUrl) {
+            launchUrl("http://webchat.freenode.net/?channels=teamkang");
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void launchUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent donate = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(donate);
+    }
+}
---
+++ src/com/roman/romcontrol/widgets/DeveloperPreference.java
@@ -0,0 +1,89 @@
+
+package com.roman.romcontrol.widgets;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.net.Uri;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.roman.romcontrol.R;
+
+public class DeveloperPreference extends Preference {
+
+    TextView mName;
+    TextView twitter;
+    TextView donateText;
+    ImageView icon;
+    LinearLayout twitterLayout;
+    LinearLayout donateLayout;
+    ImageView donateButton;
+
+    String twitterHandle;
+    String donateLink;
+    String name;
+
+    public DeveloperPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DeveloperPreference);
+        twitterHandle = a.getString(R.styleable.DeveloperPreference_twitterHandle);
+        donateLink = a.getString(R.styleable.DeveloperPreference_donateLink);
+        a.recycle();
+
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+
+        View layout = View.inflate(getContext(), R.layout.developer_preference, null);
+
+        mName = (TextView) layout.findViewById(com.android.internal.R.id.title);
+        twitter = (TextView) layout.findViewById(R.id.twitter_handle);
+        donateButton = (ImageView) layout.findViewById(R.id.donate_button);
+        twitterLayout = (LinearLayout) layout.findViewById(R.id.twitter_layout);
+        icon = (ImageView) layout.findViewById(R.id.twitter_icon);
+
+        return layout;
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+
+        twitter.setText(twitterHandle);
+        icon.setImageResource(R.drawable.twitter_bird);
+
+        if (twitterHandle == null) {
+            twitterLayout.setVisibility(View.GONE);
+        } else
+            this.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Uri uriUrl = Uri.parse("http://twitter.com/#!/" + twitterHandle);
+                    Intent twitter = new Intent(Intent.ACTION_VIEW, uriUrl);
+                    getContext().startActivity(twitter);
+                    return true;
+                }
+            });
+
+        if (donateLink == null)
+            donateButton.setVisibility(View.GONE);
+        else
+            donateButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Uri uriUrl = Uri.parse(donateLink);
+                    Intent donate = new Intent(Intent.ACTION_VIEW, uriUrl);
+                    getContext().startActivity(donate);
+                }
+            });
+
+    }
+}

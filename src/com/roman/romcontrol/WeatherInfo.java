f5b5577e4b3c5f29d9d1b6f70b0af213b9f7a6f5
sethyx <sethyx@gmail.com>
2012-03-02 10:55:53 +0100

    add direction to wind info

--- src/com/roman/romcontrol/WeatherInfo.java
+++ src/com/roman/romcontrol/WeatherInfo.java
@@ -7,16 +7,16 @@ public class WeatherInfo {
 
     private static final String NODATA = "No data";
 
-    public String city, forecast_date, condition, condition_code, temp, temp_unit, humidity, wind, speed_unit, low, high;
+    public String city, forecast_date, condition, condition_code, temp, temp_unit, humidity, wind, wind_dir, speed_unit, low, high;
 
     private Context mContext;
 
     public WeatherInfo() {
-        this.city = this.forecast_date = this.condition = this.condition_code = this.temp = this.temp_unit = this.humidity = this.wind = this.speed_unit = this.low = this.high = NODATA;
+        this.city = this.forecast_date = this.condition = this.condition_code = this.temp = this.temp_unit = this.humidity = this.wind = this.wind_dir = this.speed_unit = this.low = this.high = NODATA;
     }
 
     public WeatherInfo(String city, String fdate, String condition,String condition_code, String temp, String temp_unit, String humidity,
-            String wind, String speed_unit, String low, String high) {
+            String wind, String wind_dir, String speed_unit, String low, String high) {
         this.city = city;
         this.forecast_date = fdate;
         this.condition = condition;
@@ -24,9 +24,31 @@ public class WeatherInfo {
         this.temp = temp + "°" + temp_unit;
         this.temp_unit = temp_unit;
         this.humidity = humidity + "%";
-        this.wind = wind + speed_unit;
+        this.wind = calcDirection(wind_dir) + " " + wind + speed_unit;
         this.speed_unit = speed_unit;
         this.low = low + "°" + temp_unit;
         this.high = high + "°" + temp_unit;
     }
+    
+    private String calcDirection(String degrees) {
+        int deg = Integer.parseInt(degrees);
+        if (deg >= 338 || deg <= 22)
+            return "N";
+        else if (deg < 68)
+            return "NE";
+        else if (deg < 113)
+            return "E";
+        else if (deg < 158)
+            return "SE";
+        else if (deg < 203)
+            return "S";
+        else if (deg < 248)
+            return "SW";
+        else if (deg < 293)
+            return "W";
+        else if (deg < 338)
+            return "NW";
+        else return "";
+    }
 }
+

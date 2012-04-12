4bf0f93d839ba9c06b8f3383b302a2a7e54d3e7a
Roman Birg <romanbirg@gmail.com>
2012-04-12 03:41:58 -0500

    run signal fix for tuna devices DO NOT MERGE
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- src/com/aokp/romcontrol/service/BootReceiver.java
+++ src/com/aokp/romcontrol/service/BootReceiver.java
@@ -4,11 +4,20 @@ package com.aokp.romcontrol.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+import com.aokp.romcontrol.util.CMDProcessor;
 
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
         Intent i = new Intent(context, BootService.class);
         context.startService(i);
+
+        if (Build.BOARD.equals("tuna")) {
+            Log.i("ROMControl", "tuna board detected, executing signal fix");
+            new CMDProcessor().su.run("echo 0 > /sys/kernel/debug/smartreflex/sr_core/autocomp");
+        }
     }
 }

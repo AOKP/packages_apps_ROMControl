4618a5540219c35d1d1e2aca10c81402500b6f19
Roman Birg <romanbirg@gmail.com>
2012-04-27 02:46:41 -0500

    speed up opening of init.d tweaks
    Change-Id: I0b25e224aac0af0c0c718c8799b2dfc28f0e3432
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- src/com/aokp/romcontrol/fragments/InitD.java
+++ src/com/aokp/romcontrol/fragments/InitD.java
@@ -51,6 +51,10 @@ public class InitD extends AOKPPreferenceFragment implements
 
             super.onCreate(savedInstanceState);
 
+            // get su permissions faster so there's no hangup while
+            // looking up all of the preferences
+            new CMDProcessor().su.run("");
+
             PreferenceManager.getDefaultSharedPreferences(getActivity())
                     .registerOnSharedPreferenceChangeListener(this);
 

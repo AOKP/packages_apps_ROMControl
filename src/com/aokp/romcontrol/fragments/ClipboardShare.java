ef40224e5e89ecf661be264048b93b2a1a22c175
Prateek <f2prateek@gmail.com>
2012-06-12 22:57:29 -0600

    Share to clipboard
    Patch-sets 2 to 4: fix trailing whitespaces
    http://rootzwiki.com/topic/22533-request-thread-post-requests-here/page__st__220
    Patch-set 6: fix class name mismatch
    Requested feature. Adds option to share to clipboard.
    Signed-off-by: Prateek <f2prateek@gmail.com>
    
    Change-Id: I6fd95a06d842296d114d1f309730392cf82889b8

---
+++ src/com/aokp/romcontrol/fragments/ClipboardShare.java
@@ -0,0 +1,31 @@
+package com.aokp.romcontrol.fragments;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.ClipboardManager;
+import android.widget.Toast;
+
+public class ClipboardShare extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // get the clipboard system service
+        ClipboardManager mClipboardManager = (ClipboardManager) this.getSystemService(CLIPBOARD_SERVICE);
+
+        // get the intent
+        Intent intent = getIntent();
+
+        // get the text
+        CharSequence text = intent.getCharSequenceExtra(Intent.EXTRA_TEXT);
+
+        // put the text on the clipboard
+        mClipboardManager.setText(text);
+
+        // alert the user
+        Toast.makeText(this, R.string.clipboard_notifcation, Toast.LENGTH_SHORT).show();
+
+        finish();
+    }
+}

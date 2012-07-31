37f9420f4de7fc8a9104244992ab7481709e9632
Adam Fisch <adammfisch@gmail.com>
2012-07-31 15:56:51 -0400

    ROMControl: Upgraded About
    Improved about section.... SEXY!
    Images are pulled from your twitter account profile
    
    Patch #2
      - put back in strings i removed on accident...
    
    Patch #3
      - use url image helper (https://github.com/koush/UrlImageViewHelper)
      to handle downloading and caching profile pictures.
      - add more aokp fellas
      - randomly sort order in which devs are shown
    
    Patch #4
      - add more donate links, still missing Nitroz's
    
    Patch #5
      - add stevespearderp
    
    Patch #6
      - fix kwes' url
      - add birdman
    
    Change-Id: I420d29689e0530db8d805c06529801e1b6ea6ba4
    Signed-off-by: Adam Fisch <adammfisch@gmail.com>

--- src/com/aokp/romcontrol/widgets/DeveloperPreference.java
+++ src/com/aokp/romcontrol/widgets/DeveloperPreference.java
@@ -4,51 +4,69 @@ package com.aokp.romcontrol.widgets;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
 import android.preference.Preference;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.aokp.romcontrol.R;
+import com.koushikdutta.urlimageviewhelper.UrlImageViewCallback;
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.params.HttpClientParams;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 
 public class DeveloperPreference extends Preference {
 
-    TextView mName;
-    TextView twitter;
-    TextView donateText;
-    ImageView icon;
-    LinearLayout twitterLayout;
-    LinearLayout donateLayout;
-    ImageView donateButton;
+    private static final String TAG = "DeveloperPreference";
 
-    String twitterHandle;
-    String donateLink;
-    String name;
+    private ImageView twitterButton;
+    private ImageView donateButton;
+    private ImageView photoView;
+
+    private TextView devName;
+
+    private String nameDev;
+    private String twitterName;
+    private String donateLink;
 
     public DeveloperPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DeveloperPreference);
-        twitterHandle = a.getString(R.styleable.DeveloperPreference_twitterHandle);
+        nameDev = a.getString(R.styleable.DeveloperPreference_nameDev);
+        twitterName = a.getString(R.styleable.DeveloperPreference_twitterHandle);
         donateLink = a.getString(R.styleable.DeveloperPreference_donateLink);
         a.recycle();
-
     }
 
     @Override
     protected View onCreateView(ViewGroup parent) {
+        super.onCreateView(parent);
 
-        View layout = View.inflate(getContext(), R.layout.developer_preference, null);
+        View layout = View.inflate(getContext(), R.layout.dev_card, null);
 
-        mName = (TextView) layout.findViewById(com.android.internal.R.id.title);
-        twitter = (TextView) layout.findViewById(R.id.twitter_handle);
+        twitterButton = (ImageView) layout.findViewById(R.id.twitter_button);
         donateButton = (ImageView) layout.findViewById(R.id.donate_button);
-        twitterLayout = (LinearLayout) layout.findViewById(R.id.twitter_layout);
-        icon = (ImageView) layout.findViewById(R.id.twitter_icon);
+        devName = (TextView) layout.findViewById(R.id.name);
+        photoView = (ImageView) layout.findViewById(R.id.photo);
 
         return layout;
     }
@@ -57,33 +75,48 @@ public class DeveloperPreference extends Preference {
     protected void onBindView(View view) {
         super.onBindView(view);
 
-        twitter.setText(twitterHandle);
-        icon.setImageResource(R.drawable.twitter_bird);
-
-        if (twitterHandle == null) {
-            twitterLayout.setVisibility(View.GONE);
-        } else
-            this.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    Uri uriUrl = Uri.parse("http://twitter.com/#!/" + twitterHandle);
-                    Intent twitter = new Intent(Intent.ACTION_VIEW, uriUrl);
-                    getContext().startActivity(twitter);
-                    return true;
-                }
-            });
-
-        if (donateLink == null)
-            donateButton.setVisibility(View.GONE);
-        else
-            donateButton.setOnClickListener(new View.OnClickListener() {
+        if (donateLink != null) {
+            final OnClickListener openDonate = new OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Uri uriUrl = Uri.parse(donateLink);
-                    Intent donate = new Intent(Intent.ACTION_VIEW, uriUrl);
-                    getContext().startActivity(donate);
+                    Uri donateURL = Uri.parse(donateLink);
+                    final Intent intent = new Intent(Intent.ACTION_VIEW, donateURL);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    getContext().startActivity(intent);
                 }
-            });
+            };
+
+            donateButton.setOnClickListener(openDonate);
+        } else {
+            donateButton.setVisibility(View.GONE);
+        }
+
+        if (twitterName != null) {
+            final OnPreferenceClickListener openTwitter = new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Uri twitterURL = Uri.parse("http://twitter.com/#!/" + twitterName);
+                    final Intent intent = new Intent(Intent.ACTION_VIEW, twitterURL);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    getContext().startActivity(intent);
+                    return true;
+                }
+            };
+
+            // changed to clicking the preference to open twitter
+            // it was a hit or miss to click the twitter bird
+            // twitterButton.setOnClickListener(openTwitter);
+            this.setOnPreferenceClickListener(openTwitter);
+            final String url = "http://api.twitter.com/1/users/profile_image/" + twitterName
+                    + "?size=original";
+            UrlImageViewHelper.setUrlDrawable(this.photoView, url, R.drawable.ic_null,
+                    UrlImageViewHelper.CACHE_DURATION_ONE_WEEK);
+        } else {
+            twitterButton.setVisibility(View.INVISIBLE);
+            photoView.setVisibility(View.GONE);
+        }
+
+        devName.setText(nameDev);
 
     }
 }

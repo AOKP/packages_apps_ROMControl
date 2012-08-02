02b40a0938f82c673b36f2ad6f28803fc01d0bf2
Roman Birg <romanbirg@gmail.com>
2012-08-02 07:13:33 -0500

    Add option to forcefully show the menu button (2/2)
    Change-Id: I0499baba52b921b1dc28c4cdcf219d9eef81d33f
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -15,8 +15,10 @@ public class UserInterface extends AOKPPreferenceFragment {
     public static final String TAG = "UserInterface";
 
     private static final String PREF_STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+    private static final String PREF_MENU_PERMANENT_OVERRIDE = "menu_permanent_override";
 
     CheckBoxPreference mStatusBarNotifCount;
+    CheckBoxPreference mMenuOverride;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,8 +28,15 @@ public class UserInterface extends AOKPPreferenceFragment {
 
         mStatusBarNotifCount = (CheckBoxPreference) findPreference(PREF_STATUS_BAR_NOTIF_COUNT);
         mStatusBarNotifCount.setChecked(Settings.System.getBoolean(mContext
-                .getContentResolver(), Settings.System.STATUS_BAR_NOTIF_COUNT,
-                false));
+                .getContentResolver(), Settings.System.STATUS_BAR_NOTIF_COUNT, false));
+
+        mMenuOverride = (CheckBoxPreference) findPreference(PREF_MENU_PERMANENT_OVERRIDE);
+        mMenuOverride.setChecked(Settings.System.getBoolean(mContext
+                .getContentResolver(), Settings.System.MENU_PERMANENT_OVERRIDE, false));
+        if (!hasHardwareButtons) {
+            // these devices will always have the menu button
+            getPreferenceScreen().removePreference(mMenuOverride);
+        }
     }
 
     @Override
@@ -36,7 +45,12 @@ public class UserInterface extends AOKPPreferenceFragment {
         if (preference == mStatusBarNotifCount) {
             Settings.System.putBoolean(mContext.getContentResolver(),
                     Settings.System.STATUS_BAR_NOTIF_COUNT,
-                    ((CheckBoxPreference) preference).isChecked());
+                    isCheckBoxPrefernceChecked(preference));
+            return true;
+        } else if (preference == mMenuOverride) {
+            Settings.System.putBoolean(getActivity().getContentResolver(),
+                    Settings.System.MENU_PERMANENT_OVERRIDE,
+                    isCheckBoxPrefernceChecked(preference));
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);

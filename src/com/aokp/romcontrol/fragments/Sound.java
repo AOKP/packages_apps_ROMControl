63d534b6cb10ba1d995adb22bb0ff3caf657d67c
Adam Fisch <adammfisch@gmail.com>
2012-08-05 23:36:42 -0600

    RC: Flip Vibrate updates
    Fixed battery drain - with rate decreate & sensor unreg after screen
     off timeout
    added user customization for down threshold
    added user customization for when the listener unregisters
    after the screen is turned off
    registers back on when the screen is turned back on still using the same
    variables as when it was on before
    
    Patch Set 1:
    - Still working some kinks out, wanted to push for a battery drain fix
    
    Patch Set 2:
    - Battery drain fixed!  also should help with settings being changed in
    your pocket with it turning the sensor off after a user specified time.
    
    Change-Id: I392939e12e37e4ec947c2aee298f78a35f079277
    Signed-off-by: Adam Fisch <adammfisch@gmail.com>

--- src/com/aokp/romcontrol/fragments/Sound.java
+++ src/com/aokp/romcontrol/fragments/Sound.java
@@ -1,13 +1,19 @@
 
 package com.aokp.romcontrol.fragments;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 
 import com.aokp.romcontrol.AOKPPreferenceFragment;
@@ -15,47 +21,81 @@ import com.aokp.romcontrol.R;
 import com.aokp.romcontrol.service.FlipService;
 import com.aokp.romcontrol.service.HeadphoneService;
 
-public class Sound extends AOKPPreferenceFragment {
+public class Sound extends AOKPPreferenceFragment 
+        implements OnPreferenceChangeListener {
 
     private static final String PREF_ENABLE_VOLUME_OPTIONS = "enable_volume_options";
     private static final String PREF_HEADPHONES_PLUGGED_ACTION = "headphone_audio_mode";
     private static final String PREF_BT_CONNECTED_ACTION = "bt_audio_mode";
     private static final String PREF_FLIP_ACTION = "flip_mode";
-
+    private static final String PREF_USER_TIMEOUT = "user_timeout";
+    private static final String PREF_USER_DOWN_MS = "user_down_ms";
+    
+    SharedPreferences prefs;   
     CheckBoxPreference mEnableVolumeOptions;
     ListPreference mHeadphonesPluggedAction;
     ListPreference mBTPluggedAction;
     ListPreference mFlipAction;
-
+    ListPreference mUserUpMS;
+    ListPreference mUserDownMS;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.prefs_sound);
         PreferenceManager.setDefaultValues(mContext, R.xml.prefs_sound, true);
-
+        prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        
         mEnableVolumeOptions = (CheckBoxPreference) findPreference(PREF_ENABLE_VOLUME_OPTIONS);
         mEnableVolumeOptions.setChecked(Settings.System.getBoolean(getActivity()
                 .getContentResolver(),
                 Settings.System.ENABLE_VOLUME_OPTIONS, false));
-
+        
+        mFlipAction = (ListPreference) findPreference(PREF_FLIP_ACTION);
+        mFlipAction.setOnPreferenceChangeListener(this);
+        mFlipAction.setValue((prefs.getString(PREF_FLIP_ACTION, "-1")));
+        
         if (HeadphoneService.DEBUG)
             mContext.startService(new Intent(mContext, HeadphoneService.class));
-
+        
         if (FlipService.DEBUG)
             mContext.startService(new Intent(mContext, FlipService.class));
     }
-
+    
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
             Preference preference) {
         if (preference == mEnableVolumeOptions) {
-
+            
             boolean checked = ((CheckBoxPreference) preference).isChecked();
             Settings.System.putBoolean(getActivity().getContentResolver(),
                     Settings.System.ENABLE_VOLUME_OPTIONS, checked);
             return true;
-
+            
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+    
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mFlipAction) {
+            int val = Integer.parseInt((String) newValue);
+            if (val != -1) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(getActivity());      
+                ad.setTitle(getResources().getString(R.string.flip_dialog_title));
+                ad.setMessage(getResources().getString(R.string.flip_dialog_msg));
+                ad.setPositiveButton(
+                        getResources().getString(R.string.flip_action_positive),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                        }
+                    });
+                ad.show();
+            }
+            return true;
+        }
+        return false;
     }
 }

8a536f4ee13ec869f829e5725b3a2d2b892aced1
xoomdev <xoomdevnet@gmail.com>
2012-08-06 22:47:03 -0500

    Add Performance Settings to ROMControl
    This commit readds:
    Fast Charge support
    Minfree Settings
    Voltage Settings
    CPUSettings
    
    Patch set 2: White space fixes...
    Patch set 3: rebase
    Patch set 4: REBASE
    Patch set 5: One little whitespace.... O.o
    Patch set 6: removed voltage bits from OtherSettings
    Patch set 7: Fixup a bit in the CPUSettings for the curcpu thread
    Patch set 8: rebase
    Patch set 9: More obvious voltage control strings, a little
    better layout on voltage control, fixed up to ICS text color,
    bring back some stuff that was lost in the last rebase.
    I probably broke everything.. #blamexoomdev
    
    Change-Id: I3ab73775937467e4ba03b39a5d1d21a93c9a7870

---
+++ src/com/aokp/romcontrol/fragments/performance/Voltage.java
@@ -0,0 +1,31 @@
+package com.aokp.romcontrol.fragments.performance;
+
+public class Voltage {
+    private String freq;
+    private String currentMv;
+    private String savedMv;
+
+    public void setFreq(final String freq) {
+        this.freq = freq;
+    }
+
+    public String getFreq() {
+        return freq;
+    }
+
+    public void setCurrentMV(final String currentMv) {
+        this.currentMv = currentMv;
+    }
+
+    public String getCurrentMv() {
+        return currentMv;
+    }
+
+    public void setSavedMV(final String savedMv) {
+        this.savedMv = savedMv;
+    }
+
+    public String getSavedMV() {
+        return savedMv;
+    }
+}

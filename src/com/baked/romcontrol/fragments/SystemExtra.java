db5b9536f70a5a6d53ec494bcc3942fb097ffd41
travp624 <travp624@gmail.com>
2012-08-26 19:56:59 -0500

    Custom Carrier Label (RC part - 1/2) This change is a rewrite of the ICS custom carrier label. The change allows the user to customize the carrier label on the lockscreen and the notification curtain. The customized text overrides any dynamic mode of the labels. This change has two enhancements over the ICS version:   1. Saving an empty dialog box will revert the label      to its original, stock appearance.   2. The notification curtain style is changed to allow      mixed case labels.
    Change-Id: I083f1f3d385954574ca0bfeca8f85703ff360695
    
    Also in this is a rewrite of the statusbargeneral.java making it SystemExtra.java and moving the header to the system part of romcontrol. As this will serve as a better place for misc. stuff like this.

--- src/com/baked/romcontrol/fragments/SystemExtra.java
+++ src/com/baked/romcontrol/fragments/SystemExtra.java
@@ -23,21 +23,25 @@ import android.widget.EditText;
 import com.baked.romcontrol.BAKEDPreferenceFragment;
 import com.baked.romcontrol.R;
 
-public class StatusBarGeneral extends BAKEDPreferenceFragment {
+public class SystemExtra extends BAKEDPreferenceFragment {
 
-    public static final String TAG = "UserInterface";
+    public static final String TAG = "SystemExtra";
 
     private static final String PREF_STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
     private static final String PREF_IME_SWITCHER = "ime_switcher";
+    private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 
     CheckBoxPreference mStatusBarNotifCount;
     CheckBoxPreference mShowImeSwitcher;
+    Preference mCustomLabel;
+
+    String mCustomLabelText = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.prefs_statusbar_general);
+        addPreferencesFromResource(R.xml.prefs_system_extra);
 
         mStatusBarNotifCount = (CheckBoxPreference) findPreference(PREF_STATUS_BAR_NOTIF_COUNT);
         mStatusBarNotifCount.setChecked(Settings.System.getInt(mContext
@@ -47,6 +51,19 @@ public class StatusBarGeneral extends BAKEDPreferenceFragment {
         mShowImeSwitcher = (CheckBoxPreference) findPreference(PREF_IME_SWITCHER);
         mShowImeSwitcher.setChecked(Settings.System.getBoolean(mContext.getContentResolver(),
                 Settings.System.SHOW_STATUSBAR_IME_SWITCHER, true));
+
+        mCustomLabel = findPreference(PREF_CUSTOM_CARRIER_LABEL);
+        updateCustomLabelTextSummary();
+    }
+
+    private void updateCustomLabelTextSummary() {
+        mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.CUSTOM_CARRIER_LABEL);
+        if (mCustomLabelText == null || mCustomLabelText.length() == 0) {
+            mCustomLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomLabel.setSummary(mCustomLabelText);
+        }
     }
 
     @Override
@@ -63,6 +80,36 @@ public class StatusBarGeneral extends BAKEDPreferenceFragment {
                     Settings.System.SHOW_STATUSBAR_IME_SWITCHER,
                     isCheckBoxPrefernceChecked(preference));
             return true;
+
+        } else if (preference == mCustomLabel) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(mCustomLabelText != null ? mCustomLabelText : "");
+            alert.setView(input);
+
+            alert.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = ((Spannable) input.getText()).toString();
+                    Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.CUSTOM_CARRIER_LABEL, value);
+                    updateCustomLabelTextSummary();
+                    Intent i = new Intent();
+                    i.setAction("com.aokp.romcontrol.LABEL_CHANGED");
+                    mContext.sendBroadcast(i);
+                }
+            });
+            alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }

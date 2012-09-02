61ee8a3312a47eebfe240db1aa5b6df305701377
Roman Birg <romanbirg@gmail.com>
2012-09-02 11:48:16 -0500

    update UrlImageViewHelper
    straight from https://github.com/koush/UrlImageViewHelper/tree/master/UrlImageViewHelper
    
    Change-Id: Ieba90c5b9fc6dfbb0d40044d24ff6917a3e73164
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- src/com/koushikdutta/urlimageviewhelper/UrlImageViewHelper.java
+++ src/com/koushikdutta/urlimageviewhelper/UrlImageViewHelper.java
@@ -7,32 +7,40 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Hashtable;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.params.HttpClientParams;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
+import junit.framework.Assert;
 
+import org.apache.http.NameValuePair;
+
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.net.http.AndroidHttpClient;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Looper;
+import android.provider.ContactsContract;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public final class UrlImageViewHelper {
-    private static final String LOGTAG = "UrlImageViewHelper";
+
     public static int copyStream(InputStream input, OutputStream output) throws IOException
     {
         byte[] stuff = new byte[1024];
@@ -58,11 +66,34 @@ public final class UrlImageViewHelper {
         mResources = new Resources(mgr, mMetrics, context.getResources().getConfiguration());
     }
 
-    private static BitmapDrawable loadDrawableFromStream(Context context, InputStream stream) {
+    private static Drawable loadDrawableFromStream(Context context, String url, String filename, int targetWidth, int targetHeight) {
         prepareResources(context);
-        final Bitmap bitmap = BitmapFactory.decodeStream(stream);
-        //Log.i(LOGTAG, String.format("Loaded bitmap (%dx%d).", bitmap.getWidth(), bitmap.getHeight()));
-        return new BitmapDrawable(mResources, bitmap);
+        
+//        Log.v(Constants.LOGTAG,targetWidth);
+//        Log.v(Constants.LOGTAG,targetHeight);
+        try {
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            FileInputStream stream = new FileInputStream(filename);
+            BitmapFactory.decodeStream(stream, null, o);
+            stream.close();
+            stream = new FileInputStream(filename);
+            int scale = 0;
+            while ((o.outWidth >> scale) > targetWidth || (o.outHeight >> scale) > targetHeight) {
+                Log.v(Constants.LOGTAG,"downsampling");
+                scale++;
+            }
+            o = new Options();
+            o.inSampleSize = 1 << scale;
+            final Bitmap bitmap = BitmapFactory.decodeStream(stream, null, o);
+            if (Constants.LOG_ENABLED)
+                Log.i(Constants.LOGTAG, String.format("Loaded bitmap (%dx%d).", bitmap.getWidth(), bitmap.getHeight()));
+            BitmapDrawable bd = new BitmapDrawable(mResources, bitmap);
+            return new ZombieDrawable(url, bd);
+        }
+        catch (IOException e) {
+            return null;
+        }
     }
 
     public static final int CACHE_DURATION_INFINITE = Integer.MAX_VALUE;
@@ -180,50 +211,46 @@ public final class UrlImageViewHelper {
     private static void setUrlDrawable(final Context context, final ImageView imageView, final String url, final Drawable defaultDrawable, long cacheDurationMs, final UrlImageViewCallback callback) {
         cleanup(context);
         // disassociate this ImageView from any pending downloads
-        if (imageView != null)
-            mPendingViews.remove(imageView);
-
         if (isNullOrEmpty(url)) {
             if (imageView != null)
                 imageView.setImageDrawable(defaultDrawable);
             return;
         }
 
-        final UrlImageCache cache = UrlImageCache.getInstance();
-        Drawable drawable = cache.get(url);
+        WindowManager wm = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        final int tw = display.getWidth();
+        final int th = display.getHeight();
+
+        if (mDeadCache == null)
+            mDeadCache = new UrlLruCache(getHeapSize(context) / 8);
+        Drawable drawable;
+        BitmapDrawable zd = mDeadCache.remove(url);
+        if (zd != null) {
+            // this drawable was resurrected, it should not be in the live cache
+            if (Constants.LOG_ENABLED)
+                Log.i(Constants.LOGTAG, "zombie load");
+            Assert.assertTrue(!mAllCache.contains(zd));
+            drawable = new ZombieDrawable(url, zd);
+        }
+        else {
+            drawable = mLiveCache.get(url);
+        }
+
         if (drawable != null) {
-            //Log.i(LOGTAG, "Cache hit on: " + url);
+            if (Constants.LOG_ENABLED)
+                Log.i(Constants.LOGTAG, "Cache hit on: " + url);
             if (imageView != null)
                 imageView.setImageDrawable(drawable);
             if (callback != null)
                 callback.onLoaded(imageView, drawable, url, true);
             return;
         }
+        
+        // oh noes, at this point we definitely do not have the file available in memory
+        // let's prepare for an asynchronous load of the image.
 
-        final String filename = getFilenameForUrl(url);
-
-        File file = context.getFileStreamPath(filename);
-        if (file.exists()) {
-            try {
-                if (cacheDurationMs == CACHE_DURATION_INFINITE || System.currentTimeMillis() < file.lastModified() + cacheDurationMs) {
-                    //Log.i(LOGTAG, "File Cache hit on: " + url + ". " + (System.currentTimeMillis() - file.lastModified()) + "ms old.");
-                    FileInputStream  fis = context.openFileInput(filename);
-                    drawable = loadDrawableFromStream(context, fis);
-                    fis.close();
-                    if (imageView != null)
-                        imageView.setImageDrawable(drawable);
-                    cache.put(url, drawable);
-                    if (callback != null)
-                        callback.onLoaded(imageView, drawable, url, true);
-                    return;
-                }
-                else {
-                    //Log.i(LOGTAG, "File cache has expired. Refreshing.");
-                }
-            }
-            catch (Exception ex) {
-            }
-        }
+        final String filename = context.getFileStreamPath(getFilenameForUrl(url)).getAbsolutePath();
 
         // null it while it is downloading
         if (imageView != null)
@@ -232,7 +259,8 @@ public final class UrlImageViewHelper {
         // since listviews reuse their views, we need to 
         // take note of which url this view is waiting for.
         // This may change rapidly as the list scrolls or is filtered, etc.
-        //Log.i(LOGTAG, "Waiting for " + url);
+        if (Constants.LOG_ENABLED)
+            Log.i(Constants.LOGTAG, "Waiting for " + url);
         if (imageView != null)
             mPendingViews.put(imageView, url);
 
@@ -253,65 +281,214 @@ public final class UrlImageViewHelper {
             downloads.add(imageView);
         mPendingDownloads.put(url, downloads);
 
-        AsyncTask<Void, Void, BitmapDrawable> downloader = new AsyncTask<Void, Void, BitmapDrawable>() {
+        final int targetWidth = tw <= 0 ? Integer.MAX_VALUE : tw;
+        final int targetHeight = th <= 0 ? Integer.MAX_VALUE : th;
+        final Loader loader = new Loader() {
             @Override
-            protected BitmapDrawable doInBackground(Void... params) {
-                AndroidHttpClient client = AndroidHttpClient.newInstance(context.getPackageName());
+            public void run() {
                 try {
-                    HttpGet get = new HttpGet(url);
-                    final HttpParams httpParams = new BasicHttpParams();
-                    HttpClientParams.setRedirecting(httpParams, true);
-                    get.setParams(httpParams);
-                    HttpResponse resp = client.execute(get);
-                    int status = resp.getStatusLine().getStatusCode();
-                    if(status != HttpURLConnection.HTTP_OK){
-//                        Log.i(LOGTAG, "Couldn't download image from Server: " + url + " Reason: " + resp.getStatusLine().getReasonPhrase() + " / " + status);
-                        return null;
-                    }
-                    HttpEntity entity = resp.getEntity();
-//                    Log.i(LOGTAG, url + " Image Content Length: " + entity.getContentLength());
-                    InputStream is = entity.getContent();
-                    FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE);
-                    copyStream(is, fos);
-                    fos.close();
-                    is.close();
-                    FileInputStream  fis = context.openFileInput(filename);
-                    return loadDrawableFromStream(context, fis);
+                    result = loadDrawableFromStream(context, url, filename, targetWidth, targetHeight);
                 }
                 catch (Exception ex) {
-//                    Log.e(LOGTAG, "Exception during Image download of " + url, ex);
-                    return null;
-                }
-                finally {
-                    client.close();
                 }
             }
+        };
 
-            protected void onPostExecute(BitmapDrawable result) {
-                Drawable usableResult = result;
+        final Runnable completion = new Runnable() {
+            @Override
+            public void run() {
+                Assert.assertEquals(Looper.myLooper(), Looper.getMainLooper());
+                Drawable usableResult = loader.result;
                 if (usableResult == null)
                     usableResult = defaultDrawable;
                 mPendingDownloads.remove(url);
-                cache.put(url, usableResult);
+                mLiveCache.put(url, usableResult);
                 for (ImageView iv: downloads) {
                     // validate the url it is waiting for
                     String pendingUrl = mPendingViews.get(iv);
                     if (!url.equals(pendingUrl)) {
-                        //Log.i(LOGTAG, "Ignoring out of date request to update view for " + url);
+                        if (Constants.LOG_ENABLED)
+                            Log.i(Constants.LOGTAG, "Ignoring out of date request to update view for " + url);
                         continue;
                     }
                     mPendingViews.remove(iv);
                     if (usableResult != null) {
-                        final Drawable newImage = usableResult;
-                        final ImageView imageView = iv;
-                        imageView.setImageDrawable(newImage);
+                        //                        System.out.println(String.format("imageView: %dx%d, %dx%d", imageView.getMeasuredWidth(), imageView.getMeasuredHeight(), imageView.getWidth(), imageView.getHeight()));
+                        iv.setImageDrawable(usableResult);
+//                        System.out.println(String.format("imageView: %dx%d, %dx%d", imageView.getMeasuredWidth(), imageView.getMeasuredHeight(), imageView.getWidth(), imageView.getHeight()));
                         if (callback != null)
-                            callback.onLoaded(imageView, result, url, false);
+                            callback.onLoaded(iv, loader.result, url, false);
                     }
                 }
             }
         };
-        downloader.execute();
+        
+
+        File file = new File(filename);
+        if (file.exists()) {
+            try {
+                if (cacheDurationMs == CACHE_DURATION_INFINITE || System.currentTimeMillis() < file.lastModified() + cacheDurationMs) {
+                    if (Constants.LOG_ENABLED)
+                        Log.i(Constants.LOGTAG, "File Cache hit on: " + url + ". " + (System.currentTimeMillis() - file.lastModified()) + "ms old.");
+                    
+                    AsyncTask<Void, Void, Void> fileloader = new AsyncTask<Void, Void, Void>() {
+                        protected Void doInBackground(Void[] params) {
+                            loader.run();
+                            return null;
+                        }
+                        protected void onPostExecute(Void result) {
+                            completion.run();
+                        }
+                    };
+                    executeTask(fileloader);
+                    return;
+                }
+                else {
+                    if (Constants.LOG_ENABLED)
+                        Log.i(Constants.LOGTAG, "File cache has expired. Refreshing.");
+                }
+            }
+            catch (Exception ex) {
+            }
+        }
+
+        mDownloader.download(context, url, filename, loader, completion);
+    }
+
+    private static abstract class Loader implements Runnable {
+        public Drawable result;
+    }
+
+    public static interface UrlDownloader {
+        public void download(Context context, String url, String filename, Runnable loader, Runnable completion);
+    }
+
+    private static UrlDownloader mDefaultDownloader = new UrlDownloader() {
+        @Override
+        public void download(final Context context, final String url, final String filename, final Runnable loader, final Runnable completion) {
+            AsyncTask<Void, Void, Void> downloader = new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    try {
+                        InputStream is = null;
+                        if (url.startsWith(ContactsContract.Contacts.CONTENT_URI.toString())) {
+                            ContentResolver cr = context.getContentResolver();
+                            is = ContactsContract.Contacts.openContactPhotoInputStream(cr, Uri.parse(url));
+                        }
+                        else {
+                            URL u = new URL(url);
+                            HttpURLConnection urlConnection = (HttpURLConnection)u.openConnection();
+                            
+                            if (mRequestPropertiesCallback != null) {
+                                ArrayList<NameValuePair> props = mRequestPropertiesCallback.getHeadersForRequest(context, url);
+                                if (props != null) {
+                                    for (NameValuePair pair: props) {
+                                        urlConnection.addRequestProperty(pair.getName(), pair.getValue());
+                                    }
+                                }
+                            }
+
+                            if (urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK)
+                                return null;
+                            is = urlConnection.getInputStream();
+                        }
+                        
+                        if (is != null) {
+                            FileOutputStream fos = new FileOutputStream(filename);
+                            copyStream(is, fos);
+                            fos.close();
+                            is.close();
+                        }
+                        loader.run();
+                        return null;
+                    }
+                    catch (Throwable e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                }
+
+                protected void onPostExecute(Void result) {
+                    completion.run();
+                }
+            };
+
+            executeTask(downloader);
+        }
+    };
+    
+    static public interface RequestPropertiesCallback {
+        public ArrayList<NameValuePair> getHeadersForRequest(Context context, String url);
+    }
+    
+    static private RequestPropertiesCallback mRequestPropertiesCallback;
+    static public RequestPropertiesCallback getRequestPropertiesCallback() {
+        return mRequestPropertiesCallback;
+    }
+    
+    static public void setRequestPropertiesCallback(RequestPropertiesCallback callback) {
+        mRequestPropertiesCallback = callback;
+    }
+    
+    
+    public static void useDownloader(UrlDownloader downloader) {
+        mDownloader = downloader;
+    }
+
+    public static void useDefaultDownloader() {
+        mDownloader = mDefaultDownloader;
+    }
+    
+    public static UrlDownloader getDefaultDownloader() {
+        return mDownloader;
+    }
+
+    private static UrlImageCache mLiveCache = UrlImageCache.getInstance();
+
+    private static UrlLruCache mDeadCache;
+    private static HashSet<BitmapDrawable> mAllCache = new HashSet<BitmapDrawable>();
+    private static int getHeapSize(Context context) {
+        return ((ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE)).getMemoryClass() * 1024 * 1024;
+    }
+    
+    private static class ZombieDrawable extends WrapperDrawable {
+        public ZombieDrawable(String url, BitmapDrawable drawable) {
+            super(drawable);
+            mUrl = url;
+
+            mAllCache.add(drawable);
+            mDeadCache.remove(url);
+            mLiveCache.put(url, this);
+        }
+        
+        String mUrl;
+        
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+
+            mDeadCache.put(mUrl, mDrawable);
+            mAllCache.remove(mDrawable);
+            mLiveCache.remove(mUrl);
+            if (Constants.LOG_ENABLED)
+                Log.i(Constants.LOGTAG, "Zombie GC event");
+            System.gc();
+        }
+    }
+
+
+    private static UrlDownloader mDownloader = mDefaultDownloader;
+
+    private static void executeTask(AsyncTask<Void, Void, Void> task) {
+        if (Build.VERSION.SDK_INT < Constants.HONEYCOMB)
+            task.execute();
+        else
+            executeTaskHoneycomb(task);
+    }
+    
+    @TargetApi(Constants.HONEYCOMB)
+    private static void executeTaskHoneycomb(AsyncTask<Void, Void, Void> task) {
+        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     private static Hashtable<ImageView, String> mPendingViews = new Hashtable<ImageView, String>();

3b281079734c5c35ec698db1d552d354ebb0ad21
Stevespear426 <stephen.k.spear@gmail.com>
2012-09-03 23:06:29 -0400

    Powermenu: add screenshot and airpane mode back in.
    Change-Id: Id83cc27f67e1a698b0c3ecc72b3075234d85304a

--- src/com/aokp/romcontrol/fragments/PowerMenu.java
+++ src/com/aokp/romcontrol/fragments/PowerMenu.java
@@ -15,15 +15,15 @@ import com.aokp.romcontrol.R.xml;
 public class PowerMenu extends PreferenceFragment {
 
     //private static final String PREF_POWER_SAVER = "show_power_saver";
-    //private static final String PREF_SCREENSHOT = "show_screenshot";
+    private static final String PREF_SCREENSHOT = "show_screenshot";
     //private static final String PREF_TORCH_TOGGLE = "show_torch_toggle";
-    //private static final String PREF_AIRPLANE_TOGGLE = "show_airplane_toggle";
+    private static final String PREF_AIRPLANE_TOGGLE = "show_airplane_toggle";
     private static final String PREF_NAVBAR_HIDE = "show_navbar_hide";
 
     //CheckBoxPreference mShowPowerSaver;
-    //CheckBoxPreference mShowScreenShot;
+    CheckBoxPreference mShowScreenShot;
     //CheckBoxPreference mShowTorchToggle;
-    //CheckBoxPreference mShowAirplaneToggle;
+    CheckBoxPreference mShowAirplaneToggle;
     CheckBoxPreference mShowNavBarHide;
 
     @Override
@@ -44,22 +44,21 @@ public class PowerMenu extends PreferenceFragment {
                     .setSummary("You need to enable power saver before you can see it in the power menu.");
         }
         mShowPowerSaver.setChecked(powerSaverVal != 0);
-
+       */
         mShowScreenShot = (CheckBoxPreference) findPreference(PREF_SCREENSHOT);
         mShowScreenShot.setChecked(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
                 0) == 1);
-
+       /*
         mShowTorchToggle = (CheckBoxPreference) findPreference(PREF_TORCH_TOGGLE);
         mShowTorchToggle.setChecked(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE,
                 0) == 1);
-
+       */
         mShowAirplaneToggle = (CheckBoxPreference) findPreference(PREF_AIRPLANE_TOGGLE);
         mShowAirplaneToggle.setChecked(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE,
                 1) == 1);
-        */
 
         mShowNavBarHide = (CheckBoxPreference) findPreference(PREF_NAVBAR_HIDE);
         mShowNavBarHide.setChecked(Settings.System.getBoolean(getActivity()
@@ -70,28 +69,27 @@ public class PowerMenu extends PreferenceFragment {
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
             Preference preference) {
-        /*if (preference == mShowPowerSaver) {
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.POWER_DIALOG_SHOW_POWER_SAVER,
-                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
-            return true;
-        } else if (preference == mShowScreenShot) {
+
+          if (preference == mShowScreenShot) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
-            return true;
-        } else if (preference == mShowTorchToggle) {
+      /*  } else if (preference == mShowPowerSaver) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_DIALOG_SHOW_POWER_SAVER,
+                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+            return true;*/
+      /*  } else if (preference == mShowTorchToggle) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
-            return true;
+            return true;*/
         } else if (preference == mShowAirplaneToggle) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
             return true;
-        } else */ 
-        if (preference == mShowNavBarHide) {
+        } else if (preference == mShowNavBarHide) {
             Settings.System.putBoolean(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE,
                     ((CheckBoxPreference) preference).isChecked());

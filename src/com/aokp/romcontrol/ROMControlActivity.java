a64c98126ec9e30202955294b681293b6dcf5176
Desterly <d3s7royer@gmail.com>
2012-09-04 22:46:03 -0400

    Hardware Key remapping (Part 1 of 2)
    Ported from Cynanogenmod (cm10) - original commit in Settings - http://r.cyanogenmod.com/#/c/20140/
    
    Added into ROMControl - Part 2 contains framework modifications
    
    PatchSet 2 - spacing cleanup
    PatchSet 3 - Separate HW keys strings and icon from nav bar. (BMc)
    PatchSet 4 - Give Credit to sageDieu for making the drawable
    PatchSet 5 - Fix drawable .png (NEEDS REBASE)
    PatchSet 6 - Rebase
    PatchSet 7 - Remove Header for device w/o hw keys (deviceKeys == 0 in config.xml)
                 (Will require overlay for devices to set it to "0" if no hw keys exist)
    PatchSet 8 - update commit message
    PatchSet 9 - Remove null check, int(primitive) and null(reference type) are incomparable.
    PatchSet 10 - Fix up HWKeys check to remove menu, use bool already in RC ;)
    PatchSet 11 - Add to-do list to commit msg
    PatchSet 12 - NitPick myself and simply if check for deviceKeys
    PatchSet 13 - deviceKeys is a bool.
    PatchSet 14 - Revert PS13.
    
    To-Do:
    ------
    -Add Custom App Launching for HW keys
    -Unlink physical and virtual keypresses
    
    Signed-off-by: Desterly <d3s7royer@gmail.com>
    Signed-off-by: BMc08GT <bmc08gt.teamb00sted@gmail.com>
    Signed-off by: PhantomGamers <projectsundergaming@gmail.com>
    Change-Id: I52e834213262ee8fe1b6388efed29f18cf78ee46

--- src/com/aokp/romcontrol/ROMControlActivity.java
+++ src/com/aokp/romcontrol/ROMControlActivity.java
@@ -64,6 +64,7 @@ public class ROMControlActivity extends PreferenceActivity implements ButtonBarH
         mTablet = Settings.System
                 .getBoolean(getContentResolver(), Settings.System.TABLET_UI, false);
         hasNotificationLed = getResources().getBoolean(R.bool.has_notification_led);
+        deviceKeys = getResources().getBoolean(R.bool.has_hardware_buttons);
         mVibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         defaultLocale = Locale.getDefault();
         Log.i(TAG, "defualt locale: " + defaultLocale.getDisplayName());
@@ -212,7 +213,12 @@ public class ROMControlActivity extends PreferenceActivity implements ButtonBarH
                 if (mVibrator == null || !mVibrator.hasVibrator()) {
                     target.remove(i);
                 }
+            } else if (header.id == R.id.hardware_keys) {
+                if (!deviceKeys) {
+                      target.remove(i);
+                }
             }
+
         }
         updateHeaderList(target);
         mHeaders = target;

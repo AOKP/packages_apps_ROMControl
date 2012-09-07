b1c3e36da46cf8c80b4e7107c3a3519f91b30e17
BMc08GT <bmc08gt.teamb00sted@encounterpc.com>
2012-09-07 13:28:36 -0400

    IME Toggler as NavBar target (2/2)
    All credit goes to btdk7 from RootzWiki
    
    Change-Id: I507160f140adf332d292e5df2e90f02fdbd4f414

--- src/com/aokp/romcontrol/fragments/Navbar.java
+++ src/com/aokp/romcontrol/fragments/Navbar.java
@@ -611,6 +611,8 @@ public class Navbar extends AOKPPreferenceFragment implements
                 return getResources().getDrawable(R.drawable.ic_sysbar_search);
             } else if (uri.equals("**menu**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_menu_big);
+             } else if (uri.equals("**ime**")) {
+                return getResources().getDrawable(R.drawable.ic_sysbar_ime_switcher);
             } else if (uri.equals("**kill**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_killtask);
             } else if (uri.equals("**power**")) {
@@ -653,6 +655,8 @@ public class Navbar extends AOKPPreferenceFragment implements
                 return getResources().getString(R.string.navbar_action_search);
             else if (uri.equals("**menu**"))
                 return getResources().getString(R.string.navbar_action_menu);
+            else if (uri.equals("**ime**"))
+                return getResources().getString(R.string.navbar_action_ime);
             else if (uri.equals("**kill**"))
                 return getResources().getString(R.string.navbar_action_kill);
             else if (uri.equals("**power**"))

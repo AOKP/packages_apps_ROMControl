f7aa1b6a002172e6f589490b8b790e3473a59468
Chuck Haines <chaines@codemonkeyjava.com>
2012-09-16 10:18:45 -0400

    Fixed CPUPerformance Tegra3 issue and code cleanup
    Re-fixed issues with setting the max cpu speed for tegra 3 devices.
    Cleaned up code and made the multicore code more efficient
    
    ps2: Fix missing import
    ps3: Fix whitespace
    Conflicts:
    
    	src/com/aokp/romcontrol/service/BootService.java
    
    Change-Id: Ie2380d9f310a4a48d42f709cd0c3ba9192cecfb4

--- src/com/aokp/romcontrol/performance/CPUSettings.java
+++ src/com/aokp/romcontrol/performance/CPUSettings.java
@@ -38,11 +38,13 @@ public class CPUSettings extends Fragment implements SeekBar.OnSeekBarChangeList
 
     public static final String CURRENT_CPU = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq";
     public static final String MAX_FREQ = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq";
+    public static final String TEGRA_MAX_FREQ = "/sys/module/cpu_tegra/parameters/cpu_user_cap";
     public static final String MIN_FREQ = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq";
     public static final String STEPS = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies";
     public static final String GOVERNORS_LIST = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors";
     public static final String GOVERNOR = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor";
     public static final String IO_SCHEDULER = "/sys/block/mmcblk0/queue/scheduler";
+    public static final String NUM_OF_CPUS = "/sys/devices/system/cpu/present";
 
     public static final String MAX_CPU = "max_cpu";
     public static final String MIN_CPU = "min_cpu";
@@ -67,11 +69,17 @@ public class CPUSettings extends Fragment implements SeekBar.OnSeekBarChangeList
     private CurCPUThread mCurCPUThread;
     private static SharedPreferences preferences;
 
+    private boolean mIsTegra3 = false;
+    private int mNumOfCpu = 1;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup root,
             Bundle savedInstanceState) {
         mActivity = getActivity();
         View view = inflater.inflate(R.layout.cpu_settings, root, false);
+
+        mIsTegra3 = mActivity.getResources().getBoolean(
+            com.android.internal.R.bool.config_isTegra3);
 
         preferences = PreferenceManager.getDefaultSharedPreferences(mActivity);
 
@@ -92,6 +100,35 @@ public class CPUSettings extends Fragment implements SeekBar.OnSeekBarChangeList
         String currentIo = Helpers.getIOScheduler();
         String curMaxSpeed = Helpers.readOneLine(MAX_FREQ);
         String curMinSpeed = Helpers.readOneLine(MIN_FREQ);
+
+        if (mIsTegra3) {
+            String curTegraMaxSpeed = Helpers.readOneLine(TEGRA_MAX_FREQ);
+            int curTegraMax = 0;
+            try {
+                curTegraMax = Integer.parseInt(curTegraMaxSpeed);
+                if (curTegraMax > 0) {
+                    curMaxSpeed = Integer.toString(curTegraMax);
+                }
+            } catch (NumberFormatException ex) {
+                curTegraMax = 0;
+            }
+        }
+
+        String numOfCpus = Helpers.readOneLine(NUM_OF_CPUS);
+        String[] cpuCount = numOfCpus.split("-");
+        if (cpuCount.length > 1) {
+            try {
+                int cpuStart = Integer.parseInt(cpuCount[0]);
+                int cpuEnd = Integer.parseInt(cpuCount[1]);
+
+                mNumOfCpu = cpuEnd - cpuStart + 1;
+
+                if (mNumOfCpu < 0)
+                    mNumOfCpu = 1;
+            } catch (NumberFormatException ex) {
+                mNumOfCpu = 1;
+            }
+        }
 
         mCurFreq = (TextView) view.findViewById(R.id.current_speed);
 
@@ -169,19 +206,14 @@ public class CPUSettings extends Fragment implements SeekBar.OnSeekBarChangeList
     public void onStopTrackingTouch(SeekBar seekBar) {
         // we have a break now, write the values..
         CMDProcessor cmd = new CMDProcessor();
-        cmd.su.runWaitFor("busybox echo " + mMaxFreqSetting + " > " + MAX_FREQ);
-        cmd.su.runWaitFor("busybox echo " + mMinFreqSetting + " > " + MIN_FREQ);
-        if (new File("/sys/devices/system/cpu/cpu1").isDirectory()) {
-            cmd.su.runWaitFor("busybox echo " + mMaxFreqSetting + " > " + MAX_FREQ.replace("cpu0", "cpu1"));
-            cmd.su.runWaitFor("busybox echo " + mMinFreqSetting + " > " + MIN_FREQ.replace("cpu0", "cpu1"));
+
+        for (int i = 0; i < mNumOfCpu; i++) {
+            cmd.su.runWaitFor("busybox echo " + mMaxFreqSetting + " > " + MAX_FREQ.replace("cpu0", "cpu" + i));
+            cmd.su.runWaitFor("busybox echo " + mMinFreqSetting + " > " + MIN_FREQ.replace("cpu0", "cpu" + i));
         }
-        if (new File("/sys/devices/system/cpu/cpu2").isDirectory()) {
-            cmd.su.runWaitFor("busybox echo " + mMaxFreqSetting + " > " + MAX_FREQ.replace("cpu0", "cpu2"));
-            cmd.su.runWaitFor("busybox echo " + mMinFreqSetting + " > " + MIN_FREQ.replace("cpu0", "cpu2"));
-        }
-        if (new File("/sys/devices/system/cpu/cpu3").isDirectory()) {
-            cmd.su.runWaitFor("busybox echo " + mMaxFreqSetting + " > " + MAX_FREQ.replace("cpu0", "cpu3"));
-            cmd.su.runWaitFor("busybox echo " + mMinFreqSetting + " > " + MIN_FREQ.replace("cpu0", "cpu3"));
+        
+        if (mIsTegra3) {
+            cmd.su.runWaitFor("busybox echo " + mMaxFreqSetting + " > " + TEGRA_MAX_FREQ);
         }
     }
 
@@ -189,16 +221,13 @@ public class CPUSettings extends Fragment implements SeekBar.OnSeekBarChangeList
         public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
             String selected = parent.getItemAtPosition(pos).toString();
             CMDProcessor cmd = new CMDProcessor();
-            cmd.su.runWaitFor("busybox echo " + selected + " > " + GOVERNOR);
-            if (new File("/sys/devices/system/cpu/cpu1").exists()) {
-                cmd.su.runWaitFor("busybox echo " + selected + " > " + GOVERNOR.replace("cpu0", "cpu1"));
+            
+            // do this on all cpu's since MSM can have different governors on each cpu
+            //  and it doesn't hurt other devices to do it
+            for (int i = 0; i < mNumOfCpu; i++) {
+                cmd.su.runWaitFor("busybox echo " + selected + " > " + GOVERNOR.replace("cpu0", "cpu" + i));
             }
-            if (new File("/sys/devices/system/cpu/cpu2").exists()) {
-                cmd.su.runWaitFor("busybox echo " + selected + " > " + GOVERNOR.replace("cpu0", "cpu2"));
-            }
-            if (new File("/sys/devices/system/cpu/cpu3").exists()) {
-                cmd.su.runWaitFor("busybox echo " + selected + " > " + GOVERNOR.replace("cpu0", "cpu3"));
-            }
+
             final SharedPreferences.Editor editor = preferences.edit();
             editor.putString(GOV_PREF, selected);
             editor.commit();

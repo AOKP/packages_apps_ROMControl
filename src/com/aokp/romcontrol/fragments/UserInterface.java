d8e9f190109d6233c1bf8155da2fd98c82606c7e
BMc08GT <bmc08gt.teamb00sted@gmail.com>
2012-09-28 15:49:05 -0400

    Menu UI Overflow Toggle (1/2)
    Adds the ability to show the Menu UI Overflow (3-dots) within apps, regardless of navbar being present or not
    
    Signed-off-by: BMc08GT <bmc08gt.teamb00sted@gmail.com>
    Change-Id: Ic53389b24ec3ae838d43a904febe22ef81a3bb2e

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -86,6 +86,7 @@ public class UserInterface extends AOKPPreferenceFragment {
     private static final String PREF_KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
     private static final String PREF_MODE_TABLET_UI = "mode_tabletui";
     private static final String PREF_FORCE_DUAL_PANEL = "force_dualpanel";
+    private static final String PREF_SHOW_OVERFLOW = "show_overflow";
 
     private static final int REQUEST_PICK_WALLPAPER = 201;
     private static final int REQUEST_PICK_CUSTOM_ICON = 202;
@@ -107,6 +108,7 @@ public class UserInterface extends AOKPPreferenceFragment {
     CheckBoxPreference mKillAppLongpressBack;
     ImageView view;
     TextView error;
+    CheckBoxPreference mShowActionOverflow;
     CheckBoxPreference mTabletui;
     CheckBoxPreference mDualpane;
     Preference mLcdDensity;
@@ -120,6 +122,7 @@ public class UserInterface extends AOKPPreferenceFragment {
     private int width;
     private String errormsg;
     private String bootAniPath;
+
 
     Random randomGenerator = new Random();
 
@@ -183,6 +186,11 @@ public class UserInterface extends AOKPPreferenceFragment {
 
         mKillAppLongpressBack = (CheckBoxPreference) findPreference(PREF_KILL_APP_LONGPRESS_BACK);
                 updateKillAppLongpressBackOptions();
+
+        mShowActionOverflow = (CheckBoxPreference) findPreference(PREF_SHOW_OVERFLOW);
+        mShowActionOverflow.setChecked((Settings.System.getInt(getActivity().
+                        getApplicationContext().getContentResolver(),
+                        Settings.System.UI_FORCE_OVERFLOW_BUTTON, 0) == 1));
 
         mTabletui = (CheckBoxPreference) findPreference(PREF_MODE_TABLET_UI);
         mTabletui.setChecked(Settings.System.getBoolean(mContext.getContentResolver(),
@@ -265,6 +273,19 @@ public class UserInterface extends AOKPPreferenceFragment {
                         .runWaitFor("mv /system/media/bootanimation.backup /system/media/bootanimation.zip");
                 Helpers.getMount("ro");
                 preference.setSummary("");
+            }
+            return true;
+        } else if (preference == mShowActionOverflow) {
+            boolean enabled = mShowActionOverflow.isChecked();
+            Settings.System.putInt(getContentResolver(), Settings.System.UI_FORCE_OVERFLOW_BUTTON,
+                    enabled ? 1 : 0);
+            // Show toast appropriately
+            if (enabled) {
+                Toast.makeText(getActivity(), R.string.show_overflow_toast_enable,
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getActivity(), R.string.show_overflow_toast_disable,
+                        Toast.LENGTH_LONG).show();
             }
             return true;
         } else if (preference == mTabletui) {

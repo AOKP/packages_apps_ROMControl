9fb9adafc1cf6dcd411c92f1dbb8ca537095da1a
travp624 <travp624@gmail.com>
2012-10-12 01:14:23 -0500

    NavBar: Add screenshot to navbar Addes screenshot button and longpress option to navbar
    Thanks Kwes1020 for the sweet icon.
    
    Patch Set2: Fix opacity of screenshot icon
    Patch Set3: Rebase
    Patch Set4: OCD cleanup
    
    Change-Id: I593f693044b2bdada457ebe3ca6e7e1ab35c1137
    
    original commit to aokp by Stevespear426

--- src/com/baked/romcontrol/fragments/Navbar.java
+++ src/com/baked/romcontrol/fragments/Navbar.java
@@ -651,6 +651,8 @@ public class Navbar extends BAKEDPreferenceFragment implements
                 return getResources().getDrawable(R.drawable.ic_sysbar_recent);
             } else if (uri.equals("**search**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_search);
+            } else if (uri.equals("**screenshot**")) {
+                return getResources().getDrawable(R.drawable.ic_sysbar_screenshot);
             } else if (uri.equals("**menu**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_menu_big);
              } else if (uri.equals("**ime**")) {
@@ -695,6 +697,8 @@ public class Navbar extends BAKEDPreferenceFragment implements
                 return getResources().getString(R.string.navbar_action_recents);
             else if (uri.equals("**search**"))
                 return getResources().getString(R.string.navbar_action_search);
+            else if (uri.equals("**screenshot**"))
+                return getResources().getString(R.string.navbar_action_screenshot);
             else if (uri.equals("**menu**"))
                 return getResources().getString(R.string.navbar_action_menu);
             else if (uri.equals("**ime**"))

b4d820ad1e390ae382d9b53414ecd01499fb9a63
Stevespear426 <stephen.k.spear@gmail.com>
2012-10-22 10:57:06 -0400

    Toogles: Make Toggles hidable (2/2)
    Brings back long press actions on the settings button, and
    the ability to hide the toggles. Default is click for Settings
    long click for toggles.
    
    Patch set 2: add auto hide
    
    PS3: rebase
    
    Change-Id: Ieafa9f74d1814ed62e2b10d42e82e902f2f6b352

--- src/com/aokp/romcontrol/fragments/StatusBarToggles.java
+++ src/com/aokp/romcontrol/fragments/StatusBarToggles.java
@@ -48,6 +48,8 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements OnPrefer
     private static final String PREF_TOGGLE_BTN_ALPHA = "toggle_btn_alpha";
     private static final String PREF_TOGGLE_BTN_BACKGROUND = "toggle_btn_background";
     private static final String PREF_TOGGLE_TEXT_COLOR = "toggle_text_color";
+    private static final String PREF_SETTINGS_BUTTON_BEHAVIOR = "settings_behavior";
+    private static final String PREF_TOGGLES_AUTOHIDE = "toggles_autohide";
 
     Preference mEnabledToggles;
     Preference mLayout;
@@ -60,6 +62,8 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements OnPrefer
     ColorPickerPreference mBtnEnabledColor;
     ColorPickerPreference mBtnDisabledColor;
     ColorPickerPreference mToggleTextColor;
+    CheckBoxPreference mDefaultSettingsButtonBehavior;
+    CheckBoxPreference mTogglesAutoHide;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -111,6 +115,19 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements OnPrefer
         mBtnBackground = (SeekBarPreference) findPreference(PREF_TOGGLE_BTN_BACKGROUND);
         mBtnBackground.setInitValue((int) (btnBgAlpha * 100));
         mBtnBackground.setOnPreferenceChangeListener(this);
+
+        mDefaultSettingsButtonBehavior = (CheckBoxPreference) findPreference(PREF_SETTINGS_BUTTON_BEHAVIOR);
+        mDefaultSettingsButtonBehavior.setChecked(Settings.System.getBoolean(mContext
+                .getContentResolver(), Settings.System.STATUSBAR_SETTINGS_BEHAVIOR, true));
+            if (mDefaultSettingsButtonBehavior.isChecked()) {
+                mDefaultSettingsButtonBehavior.setSummary(R.string.summary_settings_behavior_default);
+            } else {
+                mDefaultSettingsButtonBehavior.setSummary(R.string.summary_settings_behavior_reverse);
+            }
+
+        mTogglesAutoHide = (CheckBoxPreference) findPreference(PREF_TOGGLES_AUTOHIDE);
+        mTogglesAutoHide.setChecked(Settings.System.getBoolean(mContext
+                .getContentResolver(), Settings.System.STATUSBAR_TOGGLES_AUTOHIDE, false));
 
         mToggleTextColor = (ColorPickerPreference) findPreference(
                 PREF_TOGGLE_TEXT_COLOR);
@@ -179,6 +196,23 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements OnPrefer
         } else if (preference == mResetToggles) {
             Settings.System.putString(getActivity().getContentResolver(),
                     Settings.System.STATUSBAR_TOGGLES, "WIFI");
+            return true;
+        } else if (preference == mTogglesAutoHide) {
+
+            Settings.System.putBoolean(mContext.getContentResolver(),
+                    Settings.System.STATUSBAR_TOGGLES_AUTOHIDE,
+                    ((CheckBoxPreference) preference).isChecked() ? true : false);
+            return true;
+        } else if (preference == mDefaultSettingsButtonBehavior) {
+
+            Settings.System.putBoolean(mContext.getContentResolver(),
+                    Settings.System.STATUSBAR_SETTINGS_BEHAVIOR,
+                    ((CheckBoxPreference) preference).isChecked() ? true : false);
+            if (mDefaultSettingsButtonBehavior.isChecked()) {
+                mDefaultSettingsButtonBehavior.setSummary(R.string.summary_settings_behavior_default);
+            } else {
+                mDefaultSettingsButtonBehavior.setSummary(R.string.summary_settings_behavior_reverse);
+            }
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);

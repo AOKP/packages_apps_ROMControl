ecd6bc8f3e070a39ff46843b09229b3286d4d476
JBirdVegas <jbirdvegas@gmail.com>
2012-10-29 19:03:24 -0400

    DynamicChangelog
    Parse api.github.com's json output to formulate a changelog since
    date of last update
    
    Patch Set 2: Complete rewrite -Much cleaner code
      Still a WIP but you can now see about how the output will be parsed
      to the screen
    
      we now no longer annoy github instead we parse a changelog we create
      via gerrit service hook @see http://gerrit.sudoservers.com/#/c/4253/
    
    Patch Set 3:
      Almost done. Mostly fixed dialog
    
    Patch Set 4:
      Only 3 things left:
        1) try to break it :P
        2) implement the denseChangelog.sh call in gerrit merge service hook
        3) We need new imagery, arrow denoting committer, for proper dpi bins
    
    Patch Set 5:
        added comments
    
    Patch Set 6:
        Better usage of fragment backstack to make back button navigation
        perform as expected.
    
    Change-Id: I656d6a322bdad2dcde917543669056698c47fa4f

---
+++ src/com/aokp/romcontrol/github/CommitsFragment.java
@@ -0,0 +1,107 @@
+
+package com.aokp.romcontrol.github;
+
+/*
+ * Copyright (C) 2012 The Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.app.AlertDialog;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+
+import com.aokp.romcontrol.AOKPPreferenceFragment;
+import com.aokp.romcontrol.R;
+import com.aokp.romcontrol.github.Config;
+import com.aokp.romcontrol.github.GetJSONChangelogTask;
+
+/**
+ * Initial Screen shows all projects for github organization
+ */
+public class CommitsFragment extends AOKPPreferenceFragment {
+    private final String TAG = getClass().getSimpleName();
+    private static final boolean DEBUG = false;
+    private Config mConfig;
+    private Context mContext;
+    private FragmentManager mFragmentManager;
+    private static PreferenceCategory mCategory;
+    private CommitViewerDialog mAlertDialog;
+
+    private String mPath;
+
+    public static final String PATH = "path";
+    private static final String PREF_CAT = "dynamic_changelog";
+    private static final int MENU_ID_PACKAGES = 101;
+
+    public CommitsFragment(CommitViewerDialog alertDialog, FragmentManager fm, String path) {
+        this.mAlertDialog = alertDialog;
+        this.mFragmentManager = fm;
+        this.mPath = path;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mConfig = new Config();
+        mContext = getActivity().getApplicationContext();
+        mConfig.setORGANIZATION(mContext);
+        addPreferencesFromResource(R.xml.dynamic_changelog);
+        mCategory = (PreferenceCategory) findPreference(PREF_CAT);
+        setHasOptionsMenu(true);
+        if (mPath == null) {
+            if (DEBUG) Log.d(TAG, "no path sent assuming we are looking for our changelog");
+            new GetJSONChangelogTask(mContext, mCategory, mAlertDialog).execute();
+        } else {
+            if (DEBUG) Log.d(TAG, "path sent: " + mPath);
+            new GetCommitsInProjectTask(mContext, mCategory, mAlertDialog).execute(new String[] { mPath, "1" });
+        }
+    }
+
+    public void onPrepareOptionsMenu(Menu menu) {
+        // remove old menu items
+        menu.clear();
+        // add projects as option
+        menu.add(0, MENU_ID_PACKAGES, 0, getString(R.string.changelog_menu_projects_title));
+    }
+
+    /**
+     * handle Menu onClick actions
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case MENU_ID_PACKAGES:
+                DisplayProjectsListTask showProjects
+                        = new DisplayProjectsListTask(mContext,
+                                mFragmentManager,
+                                mCategory,
+                                mAlertDialog,
+                                getId());
+                showProjects.execute();
+                return true;
+            // This should never happen but just in case let the system handle the return
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+}
---
+++ src/com/aokp/romcontrol/github/DisplayProjectsListTask.java
@@ -0,0 +1,169 @@
+
+package com.aokp.romcontrol.github;
+
+/*
+ * Copyright (C) 2012 The Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+
+import com.aokp.romcontrol.R;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+/**
+ * displays all projects from github organization
+ */
+public class DisplayProjectsListTask extends AsyncTask<Void, Void, Void> {
+    private final String TAG = getClass().getSimpleName();
+    private final boolean DEBUG = false;
+
+    private CommitViewerDialog mAlertDialog;
+    private final Context mContext;
+    private final FragmentManager mFragmentManager;
+    private final PreferenceCategory mCategory;
+    private final Config mConfig;
+    private final int mId;
+
+    public DisplayProjectsListTask(Context context, FragmentManager fm, PreferenceCategory category,
+                               CommitViewerDialog alertDialog, int id) {
+        this.mContext = context;
+        this.mFragmentManager = fm;
+        this.mCategory = category;
+        this.mConfig = new Config();
+        this.mAlertDialog = alertDialog;
+        this.mId = id;
+    }
+
+    // can use UI thread here
+    protected void onPreExecute() {
+        // start with a clean view, always
+        mCategory.removeAll();
+        mCategory.setTitle(mContext.getString(R.string.loading_projects));
+    }
+
+    // worker thread
+    protected Void doInBackground(Void... unused) {
+        HttpClient httpClient = null;
+        try {
+            httpClient = new DefaultHttpClient();
+            Log.i(TAG, "requesting projects list from url: "
+                    + mConfig.REPO_URL + "?page=1&per_page=100");
+
+            // request first 30 projects
+            HttpGet requestWebsite = new HttpGet(mConfig.REPO_URL + "?page=1&per_page=100");
+            // construct that handles recieving web streams to strings
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            // hold the response in a JSONArray
+            JSONArray repoProjectsArray =
+                    new JSONArray(httpClient.execute(requestWebsite, responseHandler));
+
+            // debugging
+            if (DEBUG)
+                Log.d(TAG, "repoProjectsArray.length() is: " + repoProjectsArray.length());
+
+            int projectsReturned = repoProjectsArray.length();
+            int page = 1; // because we returned the first 100 already
+            while (projectsReturned <= 100) {
+                HttpGet moreProjects = new HttpGet(mConfig.REPO_URL
+                        + "?page=" + page++ + "&per_page=100");
+                // construct that handles recieving web streams to strings
+                ResponseHandler<String> responseHandler_ = new BasicResponseHandler();
+                // hold the response in a JSONArray
+                JSONArray moreProjectsArray = new JSONArray(
+                        httpClient.execute(moreProjects, responseHandler_));
+                loadProjectsToScreen(moreProjectsArray);
+                // drop the loop when we find a return with < 100 projects listed
+                projectsReturned = moreProjectsArray.length();
+                if (projectsReturned < 100) {
+                    break;
+                }
+            }
+        } catch (JSONException je) {
+            Log.e(TAG, "Bad json interaction...", je);
+        } catch (IOException ioe) {
+            Log.e(TAG, "IOException...", ioe);
+        } catch (NullPointerException ne) {
+            Log.e(TAG, "NullPointer...", ne);
+        } finally {
+            httpClient.getConnectionManager().shutdown();
+        }
+        return null;
+    }
+
+    private void loadProjectsToScreen(JSONArray repoProjectsArray) {
+        try {
+            // scroll through each item in array (projects in repo organization)
+            for (int i = 0; i < repoProjectsArray.length(); i++) {
+                PreferenceScreen mProject = mCategory.getPreferenceManager()
+                        .createPreferenceScreen(mContext);
+                // make an object of each repo
+                JSONObject projectsObject = (JSONObject) repoProjectsArray.get(i);
+
+                // extract info about each project
+                final String projectName = projectsObject.getString("name");
+                String projectDescription = projectsObject.getString("description");
+                int githubProjectId = projectsObject.getInt("id");
+
+                // apply info to our preference screen
+                mProject.setKey(githubProjectId + "");
+                if (projectDescription.contains("") || projectDescription == null) {
+                    mProject.setTitle(projectName);
+                    mProject.setSummary(projectDescription);
+                } else {
+                    mProject.setTitle(projectDescription);
+                    mProject.setSummary(projectName);
+                }
+
+                mProject.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference p) {
+                        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+                        CommitsFragment commitFragment
+                                = new CommitsFragment(mAlertDialog, mFragmentManager, projectName);
+                        transaction.addToBackStack(null);
+                        transaction.replace(mId, commitFragment, projectName);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+                mCategory.addPreference(mProject);
+            }
+        } catch (JSONException badJsonRequest) {
+            Log.e(TAG, "failed to parse required info about project", badJsonRequest);
+        }
+    }
+
+    // can use UI thread here
+    protected void onPostExecute(Void unused) {
+        mCategory.setTitle(mContext.getString(R.string.org_projects));
+    }
+}

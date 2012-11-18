267c775a834d6ec476dd12d46fb5e12a6c01c751
xoomdev <xoomdevnet@gmail.com>
2012-11-17 22:28:54 -0600

    Add settings for carrier label, clock styles, notif count

--- src/com/aokp/romcontrol/fragments/StatusBarClock.java
+++ src/com/aokp/romcontrol/fragments/StatusBarClock.java
@@ -1,7 +1,94 @@
 
 package com.aokp.romcontrol.fragments;
 
-import com.aokp.romcontrol.AOKPPreferenceFragment;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
 
-public class StatusBarClock extends AOKPPreferenceFragment {
+import com.aokp.romcontrol.AOKPPreferenceFragment;
+import com.aokp.romcontrol.R;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class StatusBarClock extends AOKPPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String PREF_ENABLE = "clock_style";
+    private static final String PREF_AM_PM_STYLE = "clock_am_pm_style";
+    private static final String PREF_COLOR_PICKER = "clock_color";
+    private static final String PREF_CLOCK_WEEKDAY = "clock_weekday";
+
+    ListPreference mClockStyle;
+    ListPreference mClockAmPmstyle;
+    ColorPickerPreference mColorPicker;
+    ListPreference mClockWeekday;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.title_statusbar_clock);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.prefs_statusbar_clock);
+
+        PreferenceScreen prefs = getPreferenceScreen();
+
+        mClockStyle = (ListPreference) findPreference(PREF_ENABLE);
+        mClockStyle.setOnPreferenceChangeListener(this);
+        mClockStyle.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.STATUSBAR_CLOCK_STYLE,
+                1)));
+
+        mClockAmPmstyle = (ListPreference) findPreference(PREF_AM_PM_STYLE);
+        mClockAmPmstyle.setOnPreferenceChangeListener(this);
+        mClockAmPmstyle.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.STATUSBAR_CLOCK_AM_PM_STYLE,
+                2)));
+
+        mColorPicker = (ColorPickerPreference) findPreference(PREF_COLOR_PICKER);
+        mColorPicker.setOnPreferenceChangeListener(this);
+
+        mClockWeekday = (ListPreference) findPreference(PREF_CLOCK_WEEKDAY);
+        mClockWeekday.setOnPreferenceChangeListener(this);
+        mClockWeekday.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.STATUSBAR_CLOCK_WEEKDAY,
+                0)));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean result = false;
+
+        if (preference == mClockAmPmstyle) {
+
+            int val = Integer.parseInt((String) newValue);
+            result = Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_AM_PM_STYLE, val);
+
+        } else if (preference == mClockStyle) {
+
+            int val = Integer.parseInt((String) newValue);
+            result = Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_STYLE, val);
+
+        } else if (preference == mColorPicker) {
+            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                    .valueOf(newValue)));
+            preference.setSummary(hex);
+
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_COLOR, intHex);
+            Log.e("ROMAN", intHex + "");
+        } else if (preference == mClockWeekday) {
+            int val = Integer.parseInt((String) newValue);
+            result = Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_WEEKDAY, val);
+        }
+        return result;
+    }
 }
--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -2,7 +2,9 @@
 package com.aokp.romcontrol.fragments;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
@@ -11,6 +13,9 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.text.Spannable;
+import android.view.LayoutInflater;
+import android.widget.EditText;
 
 import com.aokp.romcontrol.AOKPPreferenceFragment;
 import com.aokp.romcontrol.R;
@@ -19,9 +24,15 @@ public class UserInterface extends AOKPPreferenceFragment {
 
     public static final String TAG = "UserInterface";
 
+    private static final String PREF_STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+    private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
     private static final String PREF_USE_ALT_RESOLVER = "use_alt_resolver";
 
+    CheckBoxPreference mStatusBarNotifCount;
+    Preference mCustomLabel;
     CheckBoxPreference mUseAltResolver;
+
+    String mCustomLabelText = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,14 @@ public class UserInterface extends AOKPPreferenceFragment {
         setTitle(R.string.title_ui);
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.prefs_ui);
+
+        mStatusBarNotifCount = (CheckBoxPreference) findPreference(PREF_STATUS_BAR_NOTIF_COUNT);
+        mStatusBarNotifCount.setChecked(Settings.System.getBoolean(mContext
+                .getContentResolver(), Settings.System.STATUSBAR_NOTIF_COUNT,
+                false));
+
+        mCustomLabel = findPreference(PREF_CUSTOM_CARRIER_LABEL);
+        updateCustomLabelTextSummary();
 
         mUseAltResolver = (CheckBoxPreference) findPreference(PREF_USE_ALT_RESOLVER);
         mUseAltResolver.setChecked(Settings.System.getBoolean(mContext.getContentResolver(),
@@ -38,13 +57,56 @@ public class UserInterface extends AOKPPreferenceFragment {
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
             final Preference preference) {
-        if (preference == mUseAltResolver) {
+        if (preference == mStatusBarNotifCount) {
+            Settings.System.putBoolean(mContext.getContentResolver(),
+                    Settings.System.STATUSBAR_NOTIF_COUNT,
+                    ((CheckBoxPreference) preference).isChecked());
+            return true;
+        } else if (preference == mCustomLabel) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(mCustomLabelText != null ? mCustomLabelText : "");
+            alert.setView(input);
+
+            alert.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = ((Spannable) input.getText()).toString();
+                    Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.CUSTOM_CARRIER_LABEL, value);
+                    updateCustomLabelTextSummary();
+                    Intent i = new Intent();
+                    i.setAction("com.aokp.romcontrol.LABEL_CHANGED");
+                    mContext.sendBroadcast(i);
+                }
+            });
+            alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();
+        } else if (preference == mUseAltResolver) {
             Settings.System.putBoolean(getActivity().getContentResolver(),
                     Settings.System.ACTIVITY_RESOLVER_USE_ALT,
                     isCheckBoxPrefernceChecked(preference));
             return true;
         }
-
         return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void updateCustomLabelTextSummary() {
+        mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.CUSTOM_CARRIER_LABEL);
+        if (mCustomLabelText == null || mCustomLabelText.length() == 0) {
+            mCustomLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomLabel.setSummary(mCustomLabelText);
+        }
     }
 }

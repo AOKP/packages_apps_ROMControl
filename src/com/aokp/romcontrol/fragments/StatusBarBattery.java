d6faaa38c0f851ceba76ca17851953ae22db9c5a
xoomdev <xoomdevnet@gmail.com>
2012-11-18 01:06:23 -0600

    Add back settings for battery types

--- src/com/aokp/romcontrol/fragments/StatusBarBattery.java
+++ src/com/aokp/romcontrol/fragments/StatusBarBattery.java
@@ -1,7 +1,47 @@
 
 package com.aokp.romcontrol.fragments;
 
-import com.aokp.romcontrol.AOKPPreferenceFragment;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
-public class StatusBarBattery extends AOKPPreferenceFragment {
+import com.aokp.romcontrol.AOKPPreferenceFragment;
+import com.aokp.romcontrol.R;
+
+public class StatusBarBattery extends AOKPPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String PREF_BATT_ICON = "battery_icon_list";
+
+    ListPreference mBatteryIcon;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.title_statusbar_battery);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.prefs_statusbar_battery);
+
+        mBatteryIcon = (ListPreference) findPreference(PREF_BATT_ICON);
+        mBatteryIcon.setOnPreferenceChangeListener(this);
+        mBatteryIcon.setValue((Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.STATUSBAR_BATTERY_ICON,
+                0))
+                + "");
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mBatteryIcon) {
+
+            int val = Integer.parseInt((String) newValue);
+            return Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_BATTERY_ICON, val);
+        }
+        return false;
+    }
 }

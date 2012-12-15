8991e645fb1fba60d35f3263de0d149cbd6b0c36
Roman Birg <romanbirg@gmail.com>
2012-12-14 18:28:17 -0600

    Add internal su queue; add fcharge state changer receiver
    see ExternalCommandService class description
    
    Change-Id: Ib96b3ee516979d69383fa35689321335fe4ae95d
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

---
+++ src/com/aokp/romcontrol/service/ExternalCommandService.java
@@ -0,0 +1,27 @@
+
+package com.aokp.romcontrol.service;
+
+import com.aokp.romcontrol.util.CMDProcessor;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Helper class to queue su commands that can be run from broadcast receivers,
+ * specifically like ones defined in AndroidManifest.xml, as they are run in the
+ * main thread, and starting async operations does not guarantee execution. This
+ * class should not be accessible from outside ROMControl. Commands will be queued.
+ */
+public class ExternalCommandService extends IntentService {
+
+    public ExternalCommandService() {
+        super(ExternalCommandService.class.getSimpleName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent.hasExtra("cmd")) {
+            new CMDProcessor().su.runWaitFor(intent.getStringExtra("cmd"));
+        }
+    }
+}

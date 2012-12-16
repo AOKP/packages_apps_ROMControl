8d78cbcb5a596bb9343a03ee638a25d3541971b7
Roman Birg <romanbirg@gmail.com>
2012-12-16 16:42:24 -0600

    add menu arrow keys toggle
    Change-Id: I7d6c1fd8ab808364deade52fd6da19a5e6785fa7
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- src/com/aokp/romcontrol/fragments/Navbar.java
+++ src/com/aokp/romcontrol/fragments/Navbar.java
@@ -79,6 +79,7 @@ public class Navbar extends AOKPPreferenceFragment implements
     private static final String PREF_NAVRING_AMOUNT = "pref_navring_amount";
     private static final String ENABLE_NAVRING_LONG = "enable_navring_long";
     private static final String NAVIGATION_BAR_WIDGETS = "navigation_bar_widgets";
+    private static final String PREF_MENU_ARROWS = "navigation_bar_menu_arrow_keys";
 
     public static final int REQUEST_PICK_CUSTOM_ICON = 200;
     public static final int REQUEST_PICK_LANDSCAPE_ICON = 201;
@@ -103,6 +104,7 @@ public class Navbar extends AOKPPreferenceFragment implements
     ListPreference mNavigationBarWidth;
     SeekBarPreference mButtonAlpha;
     CheckBoxPreference mEnableNavringLong;
+    CheckBoxPreference mMenuArrowKeysCheckBox;
     Preference mConfigureWidgets;
 
     private int mPendingIconIndex = -1;
@@ -197,6 +199,10 @@ public class Navbar extends AOKPPreferenceFragment implements
         mNavigationBarWidth.setOnPreferenceChangeListener(this);
         mConfigureWidgets = findPreference(NAVIGATION_BAR_WIDGETS);
 
+        mMenuArrowKeysCheckBox = (CheckBoxPreference) findPreference(PREF_MENU_ARROWS);
+        mMenuArrowKeysCheckBox.setChecked(Settings.System.getBoolean(getContentResolver(),
+                Settings.System.NAVIGATION_BAR_MENU_ARROW_KEYS, true));
+
         refreshSettings();
         setHasOptionsMenu(true);
         updateGlowTimesSummary();
@@ -276,6 +282,11 @@ public class Navbar extends AOKPPreferenceFragment implements
             ft.addToBackStack("config_widgets");
             ft.replace(this.getId(), fragment);
             ft.commit();
+            return true;
+        } else if (preference == mMenuArrowKeysCheckBox) {
+            Settings.System.putBoolean(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_MENU_ARROW_KEYS,
+                    ((CheckBoxPreference) preference).isChecked() ? true : false);
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);

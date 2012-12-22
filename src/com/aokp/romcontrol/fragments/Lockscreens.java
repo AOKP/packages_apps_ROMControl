9347dac15f7c6da4f379b22c9a68a1906c16203e
Jubakuba <jubakuba@gmail.com>
2012-12-22 02:14:31 -0700

    RomControl: Comment Back Lockscreen Rotation
    Lockscreen Rotation was recently commented out despite being working.
    
    Change-Id: I59495bebe2d2f1c66a11927f8d1568c5bde09c86

--- src/com/aokp/romcontrol/fragments/Lockscreens.java
+++ src/com/aokp/romcontrol/fragments/Lockscreens.java
@@ -92,7 +92,7 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
     CheckBoxPreference mLockscreenAllWidgets;
     CheckBoxPreference mLockscreenUnlimitedWidgets;
     ColorPickerPreference mLockscreenTextColor;
-//    CheckBoxPreference mLockscreenAutoRotate;
+    CheckBoxPreference mLockscreenAutoRotate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -113,9 +113,9 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
 //        mQuickUnlock = (CheckBoxPreference) findPreference(PREF_QUICK_UNLOCK);
 //        mQuickUnlock.setChecked(Settings.System.getBoolean(mContext.getContentResolver(), Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL, false));
 
-//        mLockscreenAutoRotate = (CheckBoxPreference)findPreference(PREF_LOCKSCREEN_AUTO_ROTATE);
-//        mLockscreenAutoRotate.setChecked(Settings.System.getBoolean(mContext
-//                .getContentResolver(), Settings.System.LOCKSCREEN_AUTO_ROTATE, false));
+        mLockscreenAutoRotate = (CheckBoxPreference)findPreference(PREF_LOCKSCREEN_AUTO_ROTATE);
+        mLockscreenAutoRotate.setChecked(Settings.System.getBoolean(mContext
+                .getContentResolver(), Settings.System.LOCKSCREEN_AUTO_ROTATE, false));
 
         mLockscreenBattery = (CheckBoxPreference)findPreference(PREF_LOCKSCREEN_BATTERY);
         mLockscreenBattery.setChecked(Settings.System.getBoolean(getActivity().getContentResolver(),
@@ -200,11 +200,11 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
                     Settings.System.LOCKSCREEN_BATTERY,
                     ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
             return true;
-//        } else if (preference == mLockscreenAutoRotate) {
-//            Settings.System.putBoolean(mContext.getContentResolver(),
-//                    Settings.System.LOCKSCREEN_AUTO_ROTATE,
-//                    ((CheckBoxPreference) preference).isChecked());
-//            return true;
+        } else if (preference == mLockscreenAutoRotate) {
+            Settings.System.putBoolean(mContext.getContentResolver(),
+                    Settings.System.LOCKSCREEN_AUTO_ROTATE,
+                    ((CheckBoxPreference) preference).isChecked());
+            return true;
         }
         
         return super.onPreferenceTreeClick(preferenceScreen, preference);

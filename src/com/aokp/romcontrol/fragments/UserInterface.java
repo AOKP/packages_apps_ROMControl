a686f811c26e665bb5595ac8ae80807dadd2880b
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-01-08 22:38:41 +0100

    Fix custom bootanimation
    If the bootanimation is disabled and then you choose a custom bootanimation
    the build.prop setting will not be updated and so the chosen bootanimation
    will not be visible.
    
    Change-Id: I0cea49a7b8e12fb431abddbe7852c481e4dfbbb5
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -222,36 +222,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
                     ((CheckBoxPreference) preference).isChecked());
             return true;
         } else if (preference == mDisableBootAnimation) {
-            CMDProcessor term = new CMDProcessor();
-            if (!term.su.runWaitFor(
-                    "grep -q \"debug.sf.nobootanimation\" /system/build.prop")
-                    .success()) {
-                // if not add value
-                Helpers.getMount("rw");
-                term.su.runWaitFor("echo debug.sf.nobootanimation="
-                    + String.valueOf(mDisableBootAnimation.isChecked() ? 1 : 0)
-                    + " >> /system/build.prop");
-                Helpers.getMount("ro");
-            }
-            // preform bootanimation operations off UI thread
-            AbstractAsyncSuCMDProcessor processor = new AbstractAsyncSuCMDProcessor(true) {
-                @Override
-                protected void onPostExecute(String result) {
-                    if (mDisableBootAnimation.isChecked()) {
-                        // do not show same insult as last time
-                        int newInsult = randomGenerator.nextInt(mInsults.length);
-                        while (newInsult == mLastRandomInsultIndex)
-                            newInsult = randomGenerator.nextInt(mInsults.length);
-
-                        // update our static index reference
-                        mLastRandomInsultIndex = newInsult;
-                        preference.setSummary(mInsults[newInsult]);
-                    } else {
-                        preference.setSummary("");
-                    }
-                }
-            };
-            processor.execute(getBootAnimationCommand(mDisableBootAnimation.isChecked()));
+            DisableBootAnimation();
             return true;
         } else if (preference == mCustomBootAnimation) {
             PackageManager packageManager = getActivity().getPackageManager();
@@ -707,6 +678,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
         protected void onPreExecute() {
             //Update setting to reflect that boot animation is now enabled
             mDisableBootAnimation.setChecked(false);
+            DisableBootAnimation();
             dialog.dismiss();
         }
 
@@ -722,6 +694,39 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
             Helpers.getMount("ro");
             return null;
         }
+    }
+
+    private void DisableBootAnimation() {
+        CMDProcessor term = new CMDProcessor();
+        if (!term.su.runWaitFor(
+                "grep -q \"debug.sf.nobootanimation\" /system/build.prop")
+                .success()) {
+            // if not add value
+            Helpers.getMount("rw");
+            term.su.runWaitFor("echo debug.sf.nobootanimation="
+                + String.valueOf(mDisableBootAnimation.isChecked() ? 1 : 0)
+                + " >> /system/build.prop");
+            Helpers.getMount("ro");
+        }
+        // preform bootanimation operations off UI thread
+        AbstractAsyncSuCMDProcessor processor = new AbstractAsyncSuCMDProcessor(true) {
+            @Override
+            protected void onPostExecute(String result) {
+                if (mDisableBootAnimation.isChecked()) {
+                    // do not show same insult as last time
+                    int newInsult = randomGenerator.nextInt(mInsults.length);
+                    while (newInsult == mLastRandomInsultIndex)
+                        newInsult = randomGenerator.nextInt(mInsults.length);
+
+                    // update our static index reference
+                    mLastRandomInsultIndex = newInsult;
+                    mDisableBootAnimation.setSummary(mInsults[newInsult]);
+                } else {
+                    mDisableBootAnimation.setSummary("");
+                }
+            }
+        };
+        processor.execute(getBootAnimationCommand(mDisableBootAnimation.isChecked()));
     }
 
     @Override

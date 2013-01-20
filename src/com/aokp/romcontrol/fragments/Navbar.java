35400661d7431f8699bdc7f2628537ba0271cdf3
Jubakuba <jubakuba@gmail.com>
2013-01-20 12:37:25 -0700

    ROMControl: Allow Custom Icon Tinting Option (2/2)
    PS3:  I'm a moron.  Working now.
    
    For Your Consideration:
    Zaph informed me that he believes colorizing icons slows the Rom down.
    As y'all know; I upload commits because I find it entertaining to play
    with the code a little bit (albeit very frustrating).  Often I don't intend
    to even use the commits I upload.  That being said, I did see two individual
    requests for this to return.  And a few "likes" on said requests.
    You (the actual developers) will have to duke this out.
    
    Zaph.  Chime in.  I don't mind you "-1ing" a commit on the grounds of
    disagreeing with it.
    
    Change-Id: I19dd1b19a066befb0746f3c1167dddf16fd145da

--- src/com/aokp/romcontrol/fragments/Navbar.java
+++ src/com/aokp/romcontrol/fragments/Navbar.java
@@ -71,6 +71,7 @@ public class Navbar extends AOKPPreferenceFragment implements
     private static final String PREF_NAVBAR_MENU_DISPLAY = "navbar_menu_display";
     private static final String NAVIGATION_BAR_COLOR = "nav_bar_color";
     private static final String PREF_NAV_COLOR = "nav_button_color";
+    private static final String NAVIGATION_BAR_ALLCOLOR = "navigation_bar_allcolor";
     private static final String PREF_NAV_GLOW_COLOR = "nav_button_glow_color";
     private static final String PREF_GLOW_TIMES = "glow_times";
     private static final String PREF_NAVBAR_QTY = "navbar_qty";
@@ -95,6 +96,7 @@ public class Navbar extends AOKPPreferenceFragment implements
     // move these later
     ColorPickerPreference mNavigationColor;
     ColorPickerPreference mNavigationBarColor;
+    CheckBoxPreference mColorizeAllIcons;
     ColorPickerPreference mNavigationBarGlowColor;
     ListPreference mGlowTimes;
     ListPreference menuDisplayLocation;
@@ -179,6 +181,10 @@ public class Navbar extends AOKPPreferenceFragment implements
 
         mNavigationBarColor = (ColorPickerPreference) findPreference(PREF_NAV_COLOR);
         mNavigationBarColor.setOnPreferenceChangeListener(this);
+
+        mColorizeAllIcons = (CheckBoxPreference) findPreference("navigation_bar_allcolor");
+        mColorizeAllIcons.setChecked(Settings.System.getBoolean(getActivity().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_ALLCOLOR, false));
 
         mNavigationBarGlowColor = (ColorPickerPreference) findPreference(PREF_NAV_GLOW_COLOR);
         mNavigationBarGlowColor.setOnPreferenceChangeListener(this);
@@ -295,6 +301,11 @@ public class Navbar extends AOKPPreferenceFragment implements
                     Settings.System.NAVIGATION_BAR_SHOW,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
             Helpers.restartSystemUI();
+            return true;
+        } else if (preference == mColorizeAllIcons) {
+            Settings.System.putBoolean(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_ALLCOLOR,
+                    ((CheckBoxPreference) preference).isChecked() ? true : false);
             return true;
         } else if (preference == mEnableNavringLong) {
 

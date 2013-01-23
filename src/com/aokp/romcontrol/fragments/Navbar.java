0a439535136b7d9cfc19005c01a9370cd76e6392
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-01-23 16:34:43 +0100

    add recent apps action to navbar buttons (2/2)
    This will add the option to open the recent application dialog.
    This is the dialog that will show when doing <Alt><Tab> with
    a bluetooth keyboard.
    
    Change-Id: If396c4b440d9b0a6a69cec22d81d418c7ea8ce3d
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- src/com/aokp/romcontrol/fragments/Navbar.java
+++ src/com/aokp/romcontrol/fragments/Navbar.java
@@ -694,6 +694,8 @@ public class Navbar extends AOKPPreferenceFragment implements
                 return getResources().getDrawable(R.drawable.ic_sysbar_back);
             } else if (uri.equals("**recents**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_recent);
+            } else if (uri.equals("**recentapps**")) {
+                return getResources().getDrawable(R.drawable.ic_sysbar_recent_apps);
             } else if (uri.equals("**search**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_search);
             } else if (uri.equals("**screenshot**")) {
@@ -740,6 +742,8 @@ public class Navbar extends AOKPPreferenceFragment implements
                 return getResources().getString(R.string.navbar_action_back);
             else if (uri.equals("**recents**"))
                 return getResources().getString(R.string.navbar_action_recents);
+            else if (uri.equals("**recentapps**"))
+                return getResources().getString(R.string.navbar_action_recent_apps);
             else if (uri.equals("**search**"))
                 return getResources().getString(R.string.navbar_action_search);
             else if (uri.equals("**screenshot**"))

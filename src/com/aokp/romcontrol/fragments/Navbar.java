cffcc5fdb8fb6c03f31a39153217f29faaa82b94
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-01-23 16:34:43 +0100

    add recent apps GB style action to navbar buttons (2/2)
    This will add the option to open the Gingerbread style
    recent application dialog.
    This is the dialog that will show when doing <Alt><Tab> with
    a bluetooth keyboard.
    
    Change-Id: If396c4b440d9b0a6a69cec22d81d418c7ea8ce3d
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- src/com/aokp/romcontrol/fragments/Navbar.java
+++ src/com/aokp/romcontrol/fragments/Navbar.java
@@ -737,6 +737,8 @@ public class Navbar extends AOKPPreferenceFragment implements
                 return getResources().getDrawable(R.drawable.ic_sysbar_back);
             } else if (uri.equals("**recents**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_recent);
+            } else if (uri.equals("**recentsgb**")) {
+                return getResources().getDrawable(R.drawable.ic_sysbar_recent_gb);
             } else if (uri.equals("**search**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_search);
             } else if (uri.equals("**screenshot**")) {
@@ -785,6 +787,8 @@ public class Navbar extends AOKPPreferenceFragment implements
                 return getResources().getString(R.string.navbar_action_back);
             else if (uri.equals("**recents**"))
                 return getResources().getString(R.string.navbar_action_recents);
+            else if (uri.equals("**recentsgb**"))
+                return getResources().getString(R.string.navbar_action_recents_gb);
             else if (uri.equals("**search**"))
                 return getResources().getString(R.string.navbar_action_search);
             else if (uri.equals("**screenshot**"))

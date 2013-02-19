ba9a51cd5ed3c99d59ac85bc29b6a35798dd6dcb
Stevespear426 <stephen.k.spear@gmail.com>
2013-02-20 00:53:40 +0100

    RC: Navring rework.
    PS1: Better interface for users.
         Get drawables from FW.
    
    PS2: Added logic for tabletui and layouts (may need tweaking)
    
    PS3: Cleaned up strings
         Added long and short discriptions to the pop up dialogs
         Since long press action have no icon I made a friend uri toast
         message to let user know a long press action was set.
    
    PS4: simpify the dialogs a bit. still need to fix custom icons...
    
    PS5: Fixed custom Icons did more dialog work.
         only 2 things left. tabletui/landscape layouts need tweaking
         need to move target selection and enable longpress inside the new
         ui. Getting close this is pretty functional.
    
    PS6: Adjusted custom icons size.
         Moved Enable long press and amount inside the ui
         moved Navigation ring to its own header in main activity
    
    PS7: Cleanup a bit.
         fixed crashing on changing target values
         Now using a spinner and switch for target number and longpress
    
    pic https://dl.dropbox.com/u/31932935/Screenshot_2013-02-23-21-59-23.png
    
    PS8: Comment clean up.
    
    PS9: use one content resolver instead of like 12....
         Switched to enum and switch case for dialog functions
    
    PS10; rebase off new awesome action. clean up things Jon pointed out
          added icons from Nitroz
    
    all we need now is layouts for tabletui to be cleaned up
    
    Change-Id: I33342f5e8bd3b88a56fb3cbbac2390a4cef1269f

--- src/com/aokp/romcontrol/fragments/Navbar.java
+++ src/com/aokp/romcontrol/fragments/Navbar.java
@@ -79,8 +79,6 @@ public class Navbar extends AOKPPreferenceFragment implements
     private static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
     private static final String NAVIGATION_BAR_HEIGHT_LANDSCAPE = "navigation_bar_height_landscape";
     private static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
-    private static final String PREF_NAVRING_AMOUNT = "pref_navring_amount";
-    private static final String ENABLE_NAVRING_LONG = "enable_navring_long";
     private static final String NAVIGATION_BAR_WIDGETS = "navigation_bar_widgets";
     private static final String PREF_MENU_ARROWS = "navigation_bar_menu_arrow_keys";
     private static final String NAVBAR_HIDE_ENABLE = "navbar_hide_enable";
@@ -95,8 +93,6 @@ public class Navbar extends AOKPPreferenceFragment implements
 
     public static final String PREFS_NAV_BAR = "navbar";
 
-    Preference mNavRingTargets;
-
     // move these later
     ColorPickerPreference mNavigationColor;
     ColorPickerPreference mNavigationBarColor;
@@ -106,7 +102,6 @@ public class Navbar extends AOKPPreferenceFragment implements
     ListPreference menuDisplayLocation;
     ListPreference mNavBarMenuDisplay;
     ListPreference mNavBarButtonQty;
-    ListPreference mNavRingButtonQty;
     CheckBoxPreference mEnableNavigationBar;
     ListPreference mNavigationBarHeight;
     ListPreference mNavigationBarHeightLandscape;
@@ -115,7 +110,6 @@ public class Navbar extends AOKPPreferenceFragment implements
 	Preference mWidthHelp;
     SeekBarPreference mWidthPort;
     SeekBarPreference mWidthLand;
-    CheckBoxPreference mEnableNavringLong;
     CheckBoxPreference mMenuArrowKeysCheckBox;
     Preference mConfigureWidgets;
     CheckBoxPreference mNavBarHideEnable;
@@ -149,8 +143,6 @@ public class Navbar extends AOKPPreferenceFragment implements
 
         mPicker = new ShortcutPickerHelper(this, this);
 
-        mNavRingTargets = findPreference("navring_settings");
-
         menuDisplayLocation = (ListPreference) findPreference(PREF_MENU_UNLOCK);
         menuDisplayLocation.setOnPreferenceChangeListener(this);
         menuDisplayLocation.setValue(Settings.System.getInt(getActivity()
@@ -162,11 +154,6 @@ public class Navbar extends AOKPPreferenceFragment implements
         mNavBarMenuDisplay.setValue(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.MENU_VISIBILITY,
                 0) + "");
-
-        mNavRingButtonQty = (ListPreference) findPreference(PREF_NAVRING_AMOUNT);
-        mNavRingButtonQty.setOnPreferenceChangeListener(this);
-        mNavRingButtonQty.setValue(Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.SYSTEMUI_NAVRING_AMOUNT, 1) + "");
 
         mNavBarButtonQty = (ListPreference) findPreference(PREF_NAVBAR_QTY);
         mNavBarButtonQty.setOnPreferenceChangeListener(this);
@@ -194,12 +181,6 @@ public class Navbar extends AOKPPreferenceFragment implements
         mNavBarHideTimeout.setOnPreferenceChangeListener(this);
         mNavBarHideTimeout.setValue(Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.NAV_HIDE_TIMEOUT, 3000) + "");
-
-        mEnableNavringLong = (CheckBoxPreference) findPreference("enable_navring_long");
-        mEnableNavringLong.setChecked(Settings.System.getBoolean(getContentResolver(),
-                Settings.System.SYSTEMUI_NAVRING_LONG_ENABLE, false));
-
-        mPicker = new ShortcutPickerHelper(this, this);
 
         boolean hasNavBarByDefault = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_showNavigationBar);
@@ -343,20 +324,6 @@ public class Navbar extends AOKPPreferenceFragment implements
                     ((CheckBoxPreference) preference).isChecked());
             refreshSettings();
             return true;
-        } else if (preference == mEnableNavringLong) {
-
-            Settings.System.putBoolean(getActivity().getContentResolver(),
-                    Settings.System.SYSTEMUI_NAVRING_LONG_ENABLE,
-                    ((CheckBoxPreference) preference).isChecked());
-            resetNavRingLong();
-            return true;
-        } else if (preference == mNavRingTargets) {
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            NavRingTargets fragment = new NavRingTargets();
-            ft.addToBackStack("config_nav_ring");
-            ft.replace(this.getId(), fragment);
-            ft.commit();
-            return true;
         } else if (preference == mConfigureWidgets) {
             FragmentTransaction ft = getFragmentManager().beginTransaction();
             WidgetConfigurationFragment fragment = new WidgetConfigurationFragment();
@@ -383,14 +350,6 @@ public class Navbar extends AOKPPreferenceFragment implements
         } else if (preference == mNavBarMenuDisplay) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.MENU_VISIBILITY, Integer.parseInt((String) newValue));
-            return true;
-        } else if (preference == mNavRingButtonQty) {
-            int val = Integer.parseInt((String) newValue);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.SYSTEMUI_NAVRING_AMOUNT, val);
-            resetNavRing();
-            resetNavRingLong();
-            refreshSettings();
             return true;
         } else if (preference == mNavBarButtonQty) {
             int val = Integer.parseInt((String) newValue);
@@ -533,14 +492,6 @@ public class Navbar extends AOKPPreferenceFragment implements
 
         }
         return false;
-    }
-
-    public void resetNavRing() {
-            // TODO : FIXME
-    }
-
-    public void resetNavRingLong() {
-            // TODO : FIXME
     }
 
     @Override

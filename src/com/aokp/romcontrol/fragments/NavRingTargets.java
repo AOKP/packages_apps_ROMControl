a1df2f6321dfb2efc1f0bd17adb805baa32b76a3
Jubakuba <Jubakuba@gmail.com>
2013-03-23 20:43:38 -0600

    ROMControl: Vibration, NavRing, Lockscreen Arrows
    Add back arrows to these fragments.
    
    (Goddamnitsteve<3)
    
    Change-Id: I3d94724e0ab1a3e3b9b10afebd942e7edc2d5ebe

--- src/com/aokp/romcontrol/fragments/NavRingTargets.java
+++ src/com/aokp/romcontrol/fragments/NavRingTargets.java
@@ -36,6 +36,7 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.os.UserHandle;
+import android.preference.PreferenceFragment;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
@@ -61,7 +62,7 @@ import java.io.FileOutputStream;
 import java.net.URISyntaxException;
 import java.lang.NumberFormatException;
 
-public class NavRingTargets extends Fragment implements
+public class NavRingTargets extends AOKPPreferenceFragment implements
         ShortcutPickerHelper.OnPickListener, GlowPadView.OnTriggerListener {
     private static final String TAG = "NavRing";
     private static final boolean DEBUG = false;

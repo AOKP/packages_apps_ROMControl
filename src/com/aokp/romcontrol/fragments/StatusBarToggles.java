a4387fabd93fb7656fb4138fdaa8431c33ae2bc6
KhasMek <Boushh@gmail.com>
2013-04-04 18:41:07 +0200

    Toggles: Alphabetize order
      This will alphatetize the toggle list in the Change toggle
      dialog.
    
    PS3: Add check for if toggles have been previously sorted(We only need to sort it once).
    
    Signed-off-by: KhasMek <Boushh@gmail.com>
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>
    Change-Id: Ie21806f580546074dd8c3088db3ae9de4b8da43f

--- src/com/aokp/romcontrol/fragments/StatusBarToggles.java
+++ src/com/aokp/romcontrol/fragments/StatusBarToggles.java
@@ -68,6 +68,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class StatusBarToggles extends AOKPPreferenceFragment implements
@@ -94,6 +95,8 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements
     private final int PICK_CONTACT = 1;
 
     public static final int REQUEST_PICK_CUSTOM_ICON = 200;
+
+    private static boolean mTogglesAreSorted = false;
 
     private Resources mResources;
     private PackageManager mPackMan;
@@ -909,6 +912,10 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements
             String[] splitter = userToggles.split("\\|");
             for (String toggle : splitter) {
                 userEnabledToggles.add(toggle);
+            }
+            if (!mTogglesAreSorted) {
+                Collections.sort(userEnabledToggles);
+                mTogglesAreSorted = true;
             }
             return userEnabledToggles;
         } catch (Exception e) {

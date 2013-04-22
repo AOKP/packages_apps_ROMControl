c10d0a7dd9cc9ed6f0c5b73ae912c868fe46831d
Stevespear426 <stephen.k.spear@gmail.com>
2013-04-22 16:51:50 +0200

    RC: NavBar fix some selection being off by 1
    Change-Id: Ibfdc3c1f36f2f1ab59b154524ed477b1b6f4830a
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

--- src/com/aokp/romcontrol/fragments/Navbar.java
+++ src/com/aokp/romcontrol/fragments/Navbar.java
@@ -798,9 +798,9 @@ public class Navbar extends AOKPPreferenceFragment implements
                 mPicker.pickShortcut();
         } else { // This should be any other defined action.
             if (button.getPickLongPress()) {
-                button.setLongPress(AwesomeConstants.AwesomeActions()[command]);
+                button.setLongPress(mActionCodes[command]);
             } else {
-                button.setClickAction(AwesomeConstants.AwesomeActions()[command]);
+                button.setClickAction(mActionCodes[command]);
             }
         }
         refreshButtons();

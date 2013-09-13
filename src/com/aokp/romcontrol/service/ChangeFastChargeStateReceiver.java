8cb195b8ae4c9b96c7595dac56627b1eb7462aa7
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-09-13 09:19:15 +0200

    Show a notification if fastcharge is not supported
    Change-Id: I6b13c57de117b0284949c74d0899020739a04d69
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- src/com/aokp/romcontrol/service/ChangeFastChargeStateReceiver.java
+++ src/com/aokp/romcontrol/service/ChangeFastChargeStateReceiver.java
@@ -1,11 +1,17 @@
 package com.aokp.romcontrol.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
 import java.io.File;
+
+import com.aokp.romcontrol.R;
 
 public class ChangeFastChargeStateReceiver extends BroadcastReceiver {
 
@@ -14,6 +20,21 @@ public class ChangeFastChargeStateReceiver extends BroadcastReceiver {
         final String fCHargePath = context
                 .getString(com.android.internal.R.string.config_fastChargePath);
         if (fCHargePath == null || fCHargePath.isEmpty() || !new File(fCHargePath).exists()) {
+            Bitmap bm = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher);
+
+            Notification noti = new Notification.BigTextStyle(
+                    new Notification.Builder(context)
+                    .setContentTitle(context.getString(R.string.fast_charge_not_supported_title))
+                    .setTicker(context.getString(R.string.fast_charge_not_supported_title))
+                    .setSmallIcon(R.drawable.ic_stat_notify_rom_control)
+                    .setLargeIcon(bm))
+                    .bigText(context.getString(R.string.fast_charge_not_supported))
+                    .build();
+
+            NotificationManager mNotificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.notify(2, noti);
+
             Log.e("ROMControl", "Attempted to change fast charge state but it's not enabled?");
             return;
         }

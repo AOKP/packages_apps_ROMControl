c057dcf682271e38a16d0818e8b7389fd7797014
Jonathan Steadman <xxwhitehawk@gmail.com>
2013-10-04 07:34:46 -0700

    RC: respect system defaults when ticking 4G icon by default
    *The default system setting is to show the 4G icon by default.
     However devices like m7 are set up to display the LTE icon.
     Currently on a clean install, devices that show the LTE icon
     will display appropriately. However the option to show the 4G
     icon is checked by default in RC. Make the initial checking of
     the box dependent upon whichever icon is set to display by default.
    
    requires FW patch: http://gerrit.aokp.co/#/c/12878/
    
    Change-Id: I982386af179e0cc4f0d69564fd19fecd4f22fbec
    Signed-off-by: Jonathan Steadman <xxwhitehawk@gmail.com>

--- src/com/aokp/romcontrol/fragments/StatusBarSignal.java
+++ src/com/aokp/romcontrol/fragments/StatusBarSignal.java
@@ -56,9 +56,11 @@ public class StatusBarSignal extends AOKPPreferenceFragment implements
         mAltSignal.setChecked(Settings.System.getBoolean(mContentRes,
                 Settings.System.STATUSBAR_SIGNAL_CLUSTER_ALT, false));
 
-        mShow4gForLte = (CheckBoxPreference) findPreference("show_4g_for_lte");
+        boolean check4gByDefault = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_show4GForLTE);
+        mShow4gForLte = (CheckBoxPreference)findPreference("show_4g_for_lte");
         mShow4gForLte.setChecked(Settings.System.getBoolean(mContentRes,
-                Settings.System.STATUSBAR_SIGNAL_SHOW_4G_FOR_LTE, true));
+                Settings.System.STATUSBAR_SIGNAL_SHOW_4G_FOR_LTE, check4gByDefault));
 
         if (Integer.parseInt(mDbmStyletyle.getValue()) == 0) {
             mColorPicker.setEnabled(false);

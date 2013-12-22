b2bcdc4bb26708f030009f59030f2df9974e8635
Jonathan Steadman <xxwhitehawk@gmail.com>
2013-12-22 09:42:58 -0800

    RC: add section for statusbar notifications
    Change-Id: I9a06ddb29844c1399795c78a059cdddbdbe633a0
    Signed-off-by: Jonathan Steadman <xxwhitehawk@gmail.com>

---
+++ src/com/aokp/romcontrol/StatusbarSettingsFragment.java
@@ -0,0 +1,22 @@
+package com.aokp.romcontrol.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.aokp.romcontrol.R;
+
+public class StatusbarSettingsFragment extends Fragment {
+
+    public StatusbarSettingsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_statusbar_settings, container, false);
+
+        return v;
+    }
+}

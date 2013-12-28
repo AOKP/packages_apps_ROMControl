ca5d6aebeadcdb2cb79a75fa715a6a9e92bd674a
Steve Spear <stephen.k.spear@gmail.com>
2013-12-27 20:28:36 -0500

    Custom Navigation Ring RC side
    Change-Id: Ia32fd67031fa167ad72f1f9d3c9284be9f4d2975

--- src/com/aokp/romcontrol/MainActivity.java
+++ src/com/aokp/romcontrol/MainActivity.java
@@ -14,6 +14,7 @@ import com.aokp.romcontrol.fragments.AboutFragment;
 import com.aokp.romcontrol.fragments.GeneralSettingsFragment;
 import com.aokp.romcontrol.fragments.HardwareKeysFragment;
 import com.aokp.romcontrol.fragments.InstallerSettingsFragment;
+import com.aokp.romcontrol.fragments.NavRingTargets;
 import com.aokp.romcontrol.fragments.NavigationDrawerFragment;
 import com.aokp.romcontrol.fragments.StatusbarSettingsFragment;
 import com.aokp.romcontrol.fragments.TogglesTabHostFragment;
@@ -88,9 +89,11 @@ public class MainActivity extends Activity
             case 4:
                 fragment = new TogglesTabHostFragment();
                 break;
-
             case 5:
                 fragment = new InstallerSettingsFragment();
+                break;
+            case 6:
+                fragment = new NavRingTargets();
                 break;
         }
         return fragment;

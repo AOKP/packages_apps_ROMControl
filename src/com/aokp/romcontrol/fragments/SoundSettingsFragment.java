0ecb032a7346fc65ebdaa39235cc46dbafc2c302
Will DeBerry <willdeberry@gmail.com>
2013-12-30 14:33:11 +0100

    RC: Volume options panel 2/2
    P2: Fix derp
    P3: Another typo
    
    Change-Id: Ib0b99a5ab95495001843538fcbabf9269077b50f

---
+++ src/com/aokp/romcontrol/fragments/SoundSettingsFragment.java
@@ -0,0 +1,22 @@
+package com.aokp.romcontrol.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.aokp.romcontrol.R;
+
+public class SoundSettingsFragment extends Fragment {
+
+    public SoundSettingsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_sound_settings, container, false);
+
+        return v;
+    }
+}

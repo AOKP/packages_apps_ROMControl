b41a0f01823d948400ff599822d3e8e04964e3bc
Roman Birg <romanbirg@gmail.com>
2013-12-30 06:37:48 -0500

    RC: move toggle settings into a separate tab
    In order to use a FragmentTabHost, we have to be
    using the support library Activities/Fragments.
    
    Change-Id: Ic10cd59331bc72f314f5baebe28805344445d684
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- src/com/aokp/romcontrol/fragments/NavigationDrawerFragment.java
+++ src/com/aokp/romcontrol/fragments/NavigationDrawerFragment.java
@@ -2,7 +2,7 @@ package com.aokp.romcontrol.fragments;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;

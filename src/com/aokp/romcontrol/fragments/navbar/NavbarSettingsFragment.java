085e51c2d4ea14303c694a06326ea0c66f25874f
kecinzer <kecinzer@gmail.com>
2014-01-16 12:30:12 +0100

    [2/2] Navigation bar width & height
    Signed-off-by: kecinzer <kecinzer@gmail.com>
    Change-Id: I6ee0a438bc23ba7883e98ecf6fa494d1ad01fca2

--- src/com/aokp/romcontrol/fragments/navbar/NavbarSettingsFragment.java
+++ src/com/aokp/romcontrol/fragments/navbar/NavbarSettingsFragment.java
@@ -1,7 +1,9 @@
 package com.aokp.romcontrol.fragments.navbar;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,10 @@ import com.aokp.romcontrol.settings.SingleChoiceSetting;
 
 public class NavbarSettingsFragment extends Fragment implements OnSettingChangedListener {
 
+    protected Context mContext;
+
+    SingleChoiceSetting navbar_width, navbar_height, navbar_height_landscape;
+
     public NavbarSettingsFragment() {
 
     }
@@ -21,11 +27,47 @@ public class NavbarSettingsFragment extends Fragment implements OnSettingChanged
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_navbar_settings, container, false);
 
+        mContext = getActivity();
+
+        navbar_width = (SingleChoiceSetting) v.findViewById(R.id.navigation_bar_width);
+        navbar_height = (SingleChoiceSetting) v.findViewById(R.id.navigation_bar_height);
+        navbar_height_landscape = (SingleChoiceSetting) v.findViewById(R.id.navigation_bar_height_landscape);
+
+        if (!hasHWbuttons()) {
+            if (isTablet()) {
+                navbar_width.setVisibility(View.GONE);
+            } else {
+                navbar_height_landscape.setVisibility(View.GONE);
+            }
+        } else {
+            navbar_width.setVisibility(View.GONE);
+            navbar_height.setVisibility(View.GONE);
+            navbar_height_landscape.setVisibility(View.GONE);
+        }
+
         return v;
     }
 
 
     @Override
     public void onSettingChanged(String table, String key, String oldValue, String value) {
+    }
+
+    private boolean hasHWbuttons() {
+        int hardwareKeyMask = mContext.getResources()
+                .getInteger(com.android.internal.R.integer.config_deviceHardwareKeys);
+        return (hardwareKeyMask != 0);
+    }
+
+    private boolean isTablet() {
+        DisplayMetrics displayMetrics = mContext.getResources().getDisplayMetrics();
+        int widthPixels = displayMetrics.widthPixels;
+        int heightPixels = displayMetrics.heightPixels;
+        float density = displayMetrics.density;
+        if (widthPixels < heightPixels) {
+            return ((widthPixels / density) >= 600);
+        } else {
+            return ((heightPixels / density) >= 600);
+        }
     }
 }

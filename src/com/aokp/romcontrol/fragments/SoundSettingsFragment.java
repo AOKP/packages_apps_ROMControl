52feb34a4e4895e4f28c5433e1bad0d39fec5e79
kecinzer <kecinzer@gmail.com>
2014-01-29 11:41:06 +0100

    [3/3] Increasing ring volume
    Change-Id: I1eb3af89035019fbeab969cfe4ed3d70a83281c3
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- src/com/aokp/romcontrol/fragments/SoundSettingsFragment.java
+++ src/com/aokp/romcontrol/fragments/SoundSettingsFragment.java
@@ -17,22 +17,130 @@
 package com.aokp.romcontrol.fragments;
 
 import android.app.Fragment;
+import android.database.ContentObserver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.media.AudioManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import com.aokp.romcontrol.R;
+import com.aokp.romcontrol.settings.BaseSetting.OnSettingChangedListener;
+import com.aokp.romcontrol.settings.CheckboxSetting;
+import com.aokp.romcontrol.settings.SingleChoiceSetting;
 
-public class SoundSettingsFragment extends Fragment {
+public class SoundSettingsFragment extends Fragment implements OnSettingChangedListener {
+
+    protected Context mContext;
+
+    CheckboxSetting mIncreasingRing;
+    SingleChoiceSetting mIncreasingRingMinVol, mIncreasingRingInterval;
+    private AudioManager mAudioManager;
+    int mVolumeLevel;
+
+    private ContentObserver mSettingsObserver = new ContentObserver(new Handler()) {
+        @Override
+        public void onChange(boolean selfChange) {
+            updateView();
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            updateView();
+        }
+    };
 
     public SoundSettingsFragment() {
 
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mContext = getActivity();
+
+        if (hasPhoneAbility(mContext)) {
+            mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.CONTENT_URI, true, mSettingsObserver);
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_sound_settings, container, false);
 
+        mIncreasingRing = (CheckboxSetting) v.findViewById(R.id.increasing_ring);
+        mIncreasingRingMinVol = (SingleChoiceSetting) v.findViewById(R.id.increasing_ring_min_vol);
+        mIncreasingRingInterval = (SingleChoiceSetting) v.findViewById(R.id.increasing_ring_interval);
+
+        updateView();
+
         return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mIncreasingRing.setOnSettingChangedListener(this);
+    }
+
+    @Override
+    public void onSettingChanged(String table, String key, String oldValue, String value) {
+        if ("aokp".equals(table)) {
+            mIncreasingRingMinVol.setVisibility(mIncreasingRing.isChecked() ? View.VISIBLE : View.GONE);
+            mIncreasingRingInterval.setVisibility(mIncreasingRing.isChecked() ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
+    }
+
+    public void updateView() {
+        mVolumeLevel = mAudioManager.getStreamVolume(AudioManager.STREAM_RING)-1;
+        if (mVolumeLevel > 1) {
+            mFirstRun = false;
+            int[] increasing_ring_min_volume_values = getResources().getIntArray(R.array.increasing_ring_min_volume_values);
+            String[] volumeValues = new String[mVolumeLevel];
+            String[] increasing_ring_interval_entries = getResources().getStringArray(R.array.increasing_ring_min_volume_entries);
+            String[] volumeEntries = new String[mVolumeLevel];
+            for (int i = 0; i < mVolumeLevel; i++) {
+                volumeValues[i] = String.valueOf(increasing_ring_min_volume_values[i]);
+                volumeEntries[i] = increasing_ring_interval_entries[i];
+            }
+            mIncreasingRingMinVol.setEntryValues(volumeValues);
+            mIncreasingRingMinVol.setEntries(volumeEntries);
+            mIncreasingRingMinVol.updateSummary();
+
+            mIncreasingRingInterval.setEntryValues(R.array.increasing_ring_interval_values);
+            mIncreasingRingInterval.setEntries(R.array.increasing_ring_interval_entries);
+            mIncreasingRingInterval.updateSummary();
+        } else {
+            mIncreasingRing.setChecked(false);
+            mIncreasingRing.setEnabled(false);
+            mIncreasingRingMinVol.setVisibility(View.GONE);
+            mIncreasingRingInterval.setVisibility(View.GONE);
+        }
+    }
+
+    public static boolean hasPhoneAbility(Context context) {
+        TelephonyManager telephonyManager =
+                (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (telephonyManager.getPhoneType() == TelephonyManager.PHONE_TYPE_NONE) {
+            return false;
+        }
+        return true;
     }
 }

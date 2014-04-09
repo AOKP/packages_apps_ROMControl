d49cd097b7528f27895af8e9394fc00e1baecede
kecinzer <kecinzer@gmail.com>
2014-04-09 15:18:02 +0200

    [2/2] RC: Lockscreen blur
    Change-Id: I16c07116d0155d1f35aff473c2db93b9e8955c34
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- src/com/aokp/romcontrol/fragments/LockscreenSettingsFragment.java
+++ src/com/aokp/romcontrol/fragments/LockscreenSettingsFragment.java
@@ -6,8 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.aokp.romcontrol.R;
+import com.aokp.romcontrol.settings.BaseSetting.OnSettingChangedListener;
+import com.aokp.romcontrol.settings.CheckboxSetting;
+import com.aokp.romcontrol.settings.SingleChoiceSetting;
 
-public class LockscreenSettingsFragment extends Fragment {
+public class LockscreenSettingsFragment extends Fragment implements OnSettingChangedListener {
+
+    CheckboxSetting mSeeThrough;
+    SingleChoiceSetting mBlurRadius;
 
     public LockscreenSettingsFragment() {
 
@@ -17,6 +23,22 @@ public class LockscreenSettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_lockscreen_settings, container, false);
 
+        mSeeThrough = (CheckboxSetting) v.findViewById(R.id.lockscreen_see_through);
+        mBlurRadius = (SingleChoiceSetting) v.findViewById(R.id.lockscreen_blur_radius);
+
         return v;
     }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mSeeThrough.setOnSettingChangedListener(this);
+    }
+
+    @Override
+    public void onSettingChanged(String table, String key, String oldValue, String value) {
+        if (key.equals("lockscreen_see_through")) {
+            mBlurRadius.setVisibility(mSeeThrough.isChecked() ? View.VISIBLE : View.GONE);
+        }
+    }
+
 }

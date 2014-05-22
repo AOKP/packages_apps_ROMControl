25e99866323eb42e3988918b33f15beb5107e82c
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2014-05-22 16:04:59 +0200

    Lockscreen Notification: default values and improve onSettingsChanged
    Add default values that are used in the frameworks/base part.
    
    Check for the key instead of the table in onSettingsChanged.
    
    Change-Id: Ia90c3d3eb4d55933d04f9a80926de7b192259526
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- src/com/aokp/romcontrol/fragments/LockscreenSettingsFragment.java
+++ src/com/aokp/romcontrol/fragments/LockscreenSettingsFragment.java
@@ -73,28 +73,43 @@ public class LockscreenSettingsFragment extends Fragment implements OnSettingCha
 
     @Override
     public void onSettingChanged(String table, String key, String oldValue, String value) {
-        if ("aokp".equals(table)) {
+        if (key.equals("lockscreen_notifications")
+                || key.equals("lockscreen_notifications_hide_non_clearable")
+                || key.equals("lockscreen_notifications_privacy_mode")
+                || key.equals("lockscreen_notifications_expanded_view")) {
             if (mHasProximitySensor) {
-                mPocketMode.setVisibility(mLockscreenNotifications.isChecked() ? View.VISIBLE : View.GONE);
+                mPocketMode.setVisibility(mLockscreenNotifications.isChecked() ?
+                        View.VISIBLE : View.GONE);
                 // Display only if pocket mode is enabled
-                mShowAlways.setVisibility(mLockscreenNotifications.isChecked() ? (mPocketMode.isChecked() ? View.VISIBLE : View.GONE) : View.GONE);
+                mShowAlways.setVisibility(mLockscreenNotifications.isChecked() ?
+                        (mPocketMode.isChecked() ? View.VISIBLE : View.GONE) : View.GONE);
             } else {
                 mPocketMode.setVisibility(View.GONE);
                 mShowAlways.setVisibility(View.GONE);
             }
-            mWakeOnNotification.setVisibility(mLockscreenNotifications.isChecked() ? View.VISIBLE : View.GONE);
-            mHideLowPriority.setVisibility(mLockscreenNotifications.isChecked() ? View.VISIBLE : View.GONE);
-            mHideNonClearable.setVisibility(mLockscreenNotifications.isChecked() ? View.VISIBLE : View.GONE);
+            mWakeOnNotification.setVisibility(mLockscreenNotifications.isChecked() ?
+                    View.VISIBLE : View.GONE);
+            mHideLowPriority.setVisibility(mLockscreenNotifications.isChecked() ?
+                    View.VISIBLE : View.GONE);
+            mHideNonClearable.setVisibility(mLockscreenNotifications.isChecked() ?
+                    View.VISIBLE : View.GONE);
             // Display only if hide non clearable is disabled
-            mDismissAll.setVisibility(mLockscreenNotifications.isChecked() ? (mHideNonClearable.isChecked() ? View.GONE : View.VISIBLE) : View.GONE);
-            mOffsetTop.setVisibility(mLockscreenNotifications.isChecked() ? View.VISIBLE : View.GONE);
-            mNotificationHeight.setVisibility(mLockscreenNotifications.isChecked() ? View.VISIBLE : View.GONE);
-            mNotificationColor.setVisibility(mLockscreenNotifications.isChecked() ? View.VISIBLE : View.GONE);
-            mPrivacyMode.setVisibility(mLockscreenNotifications.isChecked() ? View.VISIBLE : View.GONE);
+            mDismissAll.setVisibility(mLockscreenNotifications.isChecked() ?
+                    (mHideNonClearable.isChecked() ? View.GONE : View.VISIBLE) : View.GONE);
+            mOffsetTop.setVisibility(mLockscreenNotifications.isChecked() ?
+                    View.VISIBLE : View.GONE);
+            mNotificationHeight.setVisibility(mLockscreenNotifications.isChecked() ?
+                    View.VISIBLE : View.GONE);
+            mNotificationColor.setVisibility(mLockscreenNotifications.isChecked() ?
+                    View.VISIBLE : View.GONE);
+            mPrivacyMode.setVisibility(mLockscreenNotifications.isChecked() ?
+                    View.VISIBLE : View.GONE);
             // Display only if privacy mode is disabled
-            mExpandedView.setVisibility(mLockscreenNotifications.isChecked() ? (mPrivacyMode.isChecked() ? View.GONE : View.VISIBLE) : View.GONE);
+            mExpandedView.setVisibility(mLockscreenNotifications.isChecked() ?
+                    (mPrivacyMode.isChecked() ? View.GONE : View.VISIBLE) : View.GONE);
             // Display only if expanded view is enabled
-            mExpandedViewForce.setVisibility(mLockscreenNotifications.isChecked() ? (mExpandedView.isChecked() ? View.VISIBLE : View.GONE) : View.GONE);
+            mExpandedViewForce.setVisibility(mLockscreenNotifications.isChecked() ?
+                    (mExpandedView.isChecked() ? View.VISIBLE : View.GONE) : View.GONE);
         }
 	if (key.equals("lockscreen_see_through")) {
             mBlurRadius.setVisibility(mSeeThrough.isChecked() ? View.VISIBLE : View.GONE);

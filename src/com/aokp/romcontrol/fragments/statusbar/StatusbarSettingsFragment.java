0076118782cd4ca3d457f0daa4ed844549fad9ab
tristan202 <tristan202@gmail.com>
2015-04-23 07:43:55 +0800

    ROMControl - Add back status bar color option (2/2)
    Change-Id: I188a02a0aefd48c3f25aa04f08c72e8e65c139d3

--- src/com/aokp/romcontrol/fragments/statusbar/StatusbarSettingsFragment.java
+++ src/com/aokp/romcontrol/fragments/statusbar/StatusbarSettingsFragment.java
@@ -24,7 +24,9 @@ import android.app.Fragment;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -37,7 +39,11 @@ import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -49,6 +55,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import com.aokp.romcontrol.R;
 
@@ -92,6 +100,7 @@ public class StatusbarSettingsFragment extends Fragment {
         private static final String STATUS_BAR_DATE = "status_bar_date";
         private static final String STATUS_BAR_DATE_STYLE = "status_bar_date_style";
         private static final String STATUS_BAR_DATE_FORMAT = "status_bar_date_format";
+        private static final String PREF_COLOR_PICKER = "clock_color";
         private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
         private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
@@ -102,22 +111,41 @@ public class StatusbarSettingsFragment extends Fragment {
         public static final int CLOCK_DATE_STYLE_UPPERCASE = 2;
         private static final int CUSTOM_CLOCK_DATE_FORMAT_INDEX = 18;
 
+        private static final int MENU_RESET = Menu.FIRST;
+        private static final int DLG_RESET = 0;
+
         private ListPreference mStatusBarClock;
         private ListPreference mStatusBarAmPm;
         private ListPreference mStatusBarDate;
         private ListPreference mStatusBarDateStyle;
         private ListPreference mStatusBarDateFormat;
+        private ColorPickerPreference mColorPicker;
         private ListPreference mStatusBarBattery;
         private ListPreference mStatusBarBatteryShowPercent;
+
+        private boolean mCheckPreferences;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+            createCustomView();
+        }
 
+        private PreferenceScreen createCustomView() {
+            mCheckPreferences = false;
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.fragment_statusbar_settings);
-
+            PreferenceScreen prefSet = getPreferenceScreen();
             ContentResolver resolver = getActivity().getContentResolver();
+
+            PackageManager pm = getActivity().getPackageManager();
+            Resources systemUiResources;
+            try {
+                systemUiResources = pm.getResourcesForApplication("com.android.systemui");
+            } catch (Exception e) {
+                Log.e(TAG, "can't access systemui resources",e);
+                return null;
+            }
 
             mStatusBarClock = (ListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
             mStatusBarAmPm = (ListPreference) findPreference(STATUS_BAR_AM_PM);
@@ -177,6 +205,24 @@ public class StatusbarSettingsFragment extends Fragment {
             mStatusBarBatteryShowPercent.setSummary(mStatusBarBatteryShowPercent.getEntry());
             enableStatusBarBatteryDependents(batteryStyle);
             mStatusBarBatteryShowPercent.setOnPreferenceChangeListener(this);
+
+            mColorPicker = (ColorPickerPreference) findPreference(PREF_COLOR_PICKER);
+            mColorPicker.setOnPreferenceChangeListener(this);
+            int intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                        Settings.System.STATUSBAR_CLOCK_COLOR, -2);
+            if (intColor == -2) {
+                intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
+                        "com.android.systemui:color/status_bar_clock_color", null, null));
+                mColorPicker.setSummary(getResources().getString(R.string.default_string));
+            } else {
+                String hexColor = String.format("#%08x", (0xffffffff & intColor));
+                mColorPicker.setSummary(hexColor);
+            }
+            mColorPicker.setNewPreviewColor(intColor);
+
+            setHasOptionsMenu(true);
+            mCheckPreferences = true;
+            return prefSet;
         }
 
         protected ContentResolver getContentResolver() {
@@ -206,6 +252,9 @@ public class StatusbarSettingsFragment extends Fragment {
 
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if (!mCheckPreferences) {
+                return false;
+            }
             AlertDialog dialog;
             ContentResolver resolver = getActivity().getContentResolver();
             if (preference == mStatusBarClock) {
@@ -252,7 +301,7 @@ public class StatusbarSettingsFragment extends Fragment {
                     }
                     alert.setView(input);
 
-                    alert.setPositiveButton(R.string.menu_save, new DialogInterface.OnClickListener() {
+                    alert.setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialogInterface, int whichButton) {
                             String value = input.getText().toString();
                             if (value.equals("")) {
@@ -265,7 +314,7 @@ public class StatusbarSettingsFragment extends Fragment {
                         }
                     });
 
-                    alert.setNegativeButton(R.string.menu_cancel,
+                    alert.setNegativeButton(R.string.cancel,
                         new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialogInterface, int which) {
                             return;
@@ -295,6 +344,14 @@ public class StatusbarSettingsFragment extends Fragment {
                         resolver, CMSettings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, batteryShowPercent);
                 mStatusBarBatteryShowPercent.setSummary(
                         mStatusBarBatteryShowPercent.getEntries()[index]);
+                return true;
+            } else if (preference == mColorPicker) {
+                String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                        .valueOf(newValue)));
+                preference.setSummary(hex);
+                int intHex = ColorPickerPreference.convertToColorInt(hex);
+                Settings.System.putInt(getActivity().getContentResolver(),
+                        Settings.System.STATUSBAR_CLOCK_COLOR, intHex);
                 return true;
             }
             return false;
@@ -351,6 +408,54 @@ public class StatusbarSettingsFragment extends Fragment {
                 }
             }
             mStatusBarDateFormat.setEntries(parsedDateEntries);
+        }
+
+        private void showDialogInner(int id) {
+            DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+            newFragment.setTargetFragment(this, 0);
+            newFragment.show(getFragmentManager(), "dialog " + id);
+        }
+
+        public static class MyAlertDialogFragment extends DialogFragment {
+
+            public static MyAlertDialogFragment newInstance(int id) {
+                MyAlertDialogFragment frag = new MyAlertDialogFragment();
+                Bundle args = new Bundle();
+                args.putInt("id", id);
+                frag.setArguments(args);
+                return frag;
+            }
+
+            StatusbarSettingsFragment.SettingsPreferenceFragment getOwner() {
+                return (StatusbarSettingsFragment.SettingsPreferenceFragment) getTargetFragment();
+            }
+
+            @Override
+            public Dialog onCreateDialog(Bundle savedInstanceState) {
+                int id = getArguments().getInt("id");
+                switch (id) {
+                    case DLG_RESET:
+                        return new AlertDialog.Builder(getActivity())
+                        .setTitle(R.string.reset)
+                        .setMessage(R.string.status_bar_clock_style_reset_message)
+                        .setNegativeButton(R.string.cancel, null)
+                        .setPositiveButton(R.string.ok,
+                            new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Settings.System.putInt(getActivity().getContentResolver(),
+                                    Settings.System.STATUSBAR_CLOCK_COLOR, -2);
+                                getOwner().createCustomView();
+                            }
+                        })
+                        .create();
+                }
+                throw new IllegalArgumentException("unknown id " + id);
+            }
+
+            @Override
+            public void onCancel(DialogInterface dialog) {
+
+            }
         }
     }
 }

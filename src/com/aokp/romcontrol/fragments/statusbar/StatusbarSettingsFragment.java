aced7f4fc7783cd6bae5f2f006e58742e832b958
tristan202 <tristan202@gmail.com>
2015-04-26 17:13:20 +0800

    ROMControl - enable reset option to status bar clock color
    Reference
    https://github.com/temasek/android_packages_apps_Settings/commit/fbd59b5a38dfe4a12a97eeeb1d9055a6301399c7
    
    Modified for use in cm-12.1
    
    Change-Id: I4f3f392b4c40d0808c5a025755a0eced24ddb30e

--- src/com/aokp/romcontrol/fragments/statusbar/StatusbarSettingsFragment.java
+++ src/com/aokp/romcontrol/fragments/statusbar/StatusbarSettingsFragment.java
@@ -473,5 +473,23 @@ public class StatusbarSettingsFragment extends Fragment {
 
             }
         }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            menu.add(0, MENU_RESET, 0, R.string.reset)
+                    .setIcon(R.drawable.ic_settings_reset)
+                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case MENU_RESET:
+                    showDialogInner(DLG_RESET);
+                    return true;
+                 default:
+                    return super.onContextItemSelected(item);
+            }
+        }
     }
 }

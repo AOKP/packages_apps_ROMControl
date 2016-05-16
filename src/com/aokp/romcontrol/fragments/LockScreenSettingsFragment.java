b5979856657c5040df5818ac3e2ee63ce98bed0e
STELIX <ssspinni@gmail.com>
2016-05-16 12:52:33 +0200

    Revert "Revert "ROMControl - Lockscreen weather options [2/2]""
    This reverts commit 183423852d1baa940c31cf0568bda0219dba7011.
    
    Change-Id: Ie0167e13d866b62bbc00de074b6cb1d624b0501d
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- src/com/aokp/romcontrol/fragments/LockScreenSettingsFragment.java
+++ src/com/aokp/romcontrol/fragments/LockScreenSettingsFragment.java
@@ -17,10 +17,15 @@
 package com.aokp.romcontrol.fragments;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.WallpaperManager;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.net.Uri;
 import android.content.res.Configuration;
@@ -28,10 +33,14 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -43,6 +52,7 @@ import java.util.Map;
 
 import com.aokp.romcontrol.R;
 import com.aokp.romcontrol.widgets.SeekBarPreference;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 public class LockScreenSettingsFragment extends Fragment {
 
@@ -73,18 +83,64 @@ public class LockScreenSettingsFragment extends Fragment {
         private static final String KEY_WALLPAPER_SET = "lockscreen_wallpaper_set";
         private static final String KEY_WALLPAPER_CLEAR = "lockscreen_wallpaper_clear";
 
+        private static final String PREF_CAT_COLORS =
+                "weather_cat_colors";
+        private static final String PREF_SHOW_WEATHER =
+                "weather_show_weather";
+        private static final String PREF_SHOW_LOCATION =
+                "weather_show_location";
+        private static final String PREF_SHOW_TIMESTAMP =
+                "weather_show_timestamp";
+        private static final String PREF_CONDITION_ICON =
+                "weather_condition_icon";
+        private static final String PREF_COLORIZE_ALL_ICONS =
+                "weather_colorize_all_icons";
+        private static final String PREF_TEXT_COLOR =
+                "weather_text_color";
+        private static final String PREF_ICON_COLOR =
+                "weather_icon_color";
         private static final String LOCKSCREEN_MAX_NOTIF_CONFIG =
                 "lockscreen_max_notif_cofig";
 
         private Preference mSetWallpaper;
         private Preference mClearWallpaper;
         private SeekBarPreference mMaxKeyguardNotifConfig;
+
+        private static final int MONOCHROME_ICON = 0;
+        private static final int DEFAULT_COLOR = 0xffffffff;
+        private static final int HOLO_BLUE_LIGHT = 0xff33b5e5;
+
+        private static final int MENU_RESET = Menu.FIRST;
+        private static final int DLG_RESET = 0;
+
+        private SwitchPreference mShowWeather;
+        private SwitchPreference mShowLocation;
+        private SwitchPreference mShowTimestamp;
+        private ListPreference mConditionIcon;
+        private SwitchPreference mColorizeAllIcons;
+        private ColorPickerPreference mTextColor;
+        private ColorPickerPreference mIconColor;
+
         private ContentResolver mResolver;
+
+        protected void removePreference(String key) {
+            Preference pref = findPreference(key);
+            if (pref != null) {
+                getPreferenceScreen().removePreference(pref);
+            }
+        }
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+            refreshSettings();
+        }
 
+        public void refreshSettings() {
+            PreferenceScreen prefs = getPreferenceScreen();
+            if (prefs != null) {
+                prefs.removeAll();
+            }
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.fragment_lockscreen_settings);
             mResolver = getActivity().getContentResolver();
@@ -92,7 +148,107 @@ public class LockScreenSettingsFragment extends Fragment {
             mSetWallpaper = (Preference) findPreference(KEY_WALLPAPER_SET);
             mClearWallpaper = (Preference) findPreference(KEY_WALLPAPER_CLEAR);
 
+            boolean showWeather = Settings.System.getInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHOW_WEATHER, 0) == 1;
+            int conditionIcon = Settings.System.getInt(mResolver,
+                   Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
+            boolean colorizeAllIcons = Settings.System.getInt(mResolver,
+                        Settings.System.LOCK_SCREEN_WEATHER_COLORIZE_ALL_ICONS, 0) == 1;
+
+            int intColor;
+            String hexColor;
+
+            mShowWeather =
+                    (SwitchPreference) findPreference(PREF_SHOW_WEATHER);
+            mShowWeather.setChecked(showWeather);
+            mShowWeather.setOnPreferenceChangeListener(this);
+
+            PreferenceCategory catColors =
+                    (PreferenceCategory) findPreference(PREF_CAT_COLORS);
+            mTextColor =
+                    (ColorPickerPreference) findPreference(PREF_TEXT_COLOR);
+            mIconColor =
+                    (ColorPickerPreference) findPreference(PREF_ICON_COLOR);
+
+            mMaxKeyguardNotifConfig = (SeekBarPreference) findPreference(LOCKSCREEN_MAX_NOTIF_CONFIG);
+            int kgconf = Settings.System.getInt(mResolver,
+                    Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 5);
+            mMaxKeyguardNotifConfig.setValue(kgconf);
+            mMaxKeyguardNotifConfig.setOnPreferenceChangeListener(this);
+
+            if (showWeather) {
+                mShowLocation =
+                        (SwitchPreference) findPreference(PREF_SHOW_LOCATION);
+                mShowLocation.setChecked(Settings.System.getInt(mResolver,
+                        Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION, 1) == 1);
+                mShowLocation.setOnPreferenceChangeListener(this);
+
+                mShowTimestamp =
+                        (SwitchPreference) findPreference(PREF_SHOW_TIMESTAMP);
+                mShowTimestamp.setChecked(Settings.System.getInt(mResolver,
+                        Settings.System.LOCK_SCREEN_SHOW_WEATHER_TIMESTAMP, 1) == 1);
+                mShowTimestamp.setOnPreferenceChangeListener(this);
+
+                mConditionIcon =
+                        (ListPreference) findPreference(PREF_CONDITION_ICON);
+                mConditionIcon.setValue(String.valueOf(conditionIcon));
+                mConditionIcon.setSummary(mConditionIcon.getEntry());
+                mConditionIcon.setOnPreferenceChangeListener(this);
+
+                mColorizeAllIcons =
+                        (SwitchPreference) findPreference(PREF_COLORIZE_ALL_ICONS);
+                mColorizeAllIcons.setChecked(colorizeAllIcons);
+                mColorizeAllIcons.setOnPreferenceChangeListener(this);
+
+                intColor = Settings.System.getInt(mResolver,
+                        Settings.System.LOCK_SCREEN_WEATHER_TEXT_COLOR,
+                        DEFAULT_COLOR);
+                mTextColor.setNewPreviewColor(intColor);
+                hexColor = String.format("#%08x", (0xffffffff & intColor));
+                mTextColor.setSummary(hexColor);
+                mTextColor.setOnPreferenceChangeListener(this);
+            } else {
+                removePreference(PREF_SHOW_LOCATION);
+                removePreference(PREF_SHOW_TIMESTAMP);
+                removePreference(PREF_CONDITION_ICON);
+                removePreference(PREF_COLORIZE_ALL_ICONS);
+                catColors.removePreference(mTextColor);
+            }
+            if (showWeather && ((conditionIcon == MONOCHROME_ICON)
+                    || (conditionIcon != MONOCHROME_ICON && colorizeAllIcons))) {
+                intColor = Settings.System.getInt(mResolver,
+                        Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR,
+                        DEFAULT_COLOR);
+                mIconColor.setNewPreviewColor(intColor);
+                hexColor = String.format("#%08x", (0xffffffff & intColor));
+                mIconColor.setSummary(hexColor);
+                mIconColor.setOnPreferenceChangeListener(this);
+            } else {
+                catColors.removePreference(mIconColor);
+                if (!showWeather) {
+                    removePreference(PREF_CAT_COLORS);
+                }
+            }
+
             setHasOptionsMenu(true);
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            menu.add(0, MENU_RESET, 0, R.string.reset)
+                    .setIcon(R.drawable.ic_settings_reset)
+                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case MENU_RESET:
+                    showDialogInner(DLG_RESET);
+                    return true;
+                 default:
+                    return super.onContextItemSelected(item);
+            }
         }
 
         @Override
@@ -102,8 +258,121 @@ public class LockScreenSettingsFragment extends Fragment {
                 Settings.System.putInt(mResolver,
                         Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, kgconf);
                 return true;
+            } else if (preference == mShowWeather) {
+                value = (Boolean) newValue;
+                Settings.System.putInt(mResolver,
+                      Settings.System.LOCK_SCREEN_SHOW_WEATHER,
+                      value ? 1 : 0);
+                refreshSettings();
+                return true;
+            } else if (preference == mShowLocation) {
+                value = (Boolean) newValue;
+                Settings.System.putInt(mResolver,
+                        Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION,
+                        value ? 1 : 0);
+                return true;
+            } else if (preference == mShowTimestamp) {
+                value = (Boolean) newValue;
+                Settings.System.putInt(mResolver,
+                        Settings.System.LOCK_SCREEN_SHOW_WEATHER_TIMESTAMP,
+                    value ? 1 : 0);
+                return true;
+            } else if (preference == mConditionIcon) {
+                int intValue = Integer.valueOf((String) newValue);
+                int index = mConditionIcon.findIndexOfValue((String) newValue);
+                Settings.System.putInt(mResolver,
+                        Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, intValue);
+                mConditionIcon.setSummary(mConditionIcon.getEntries()[index]);
+                refreshSettings();
+                return true;
+            } else if (preference == mColorizeAllIcons) {
+                value = (Boolean) newValue;
+                Settings.System.putInt(mResolver,
+                        Settings.System.LOCK_SCREEN_WEATHER_COLORIZE_ALL_ICONS,
+                        value ? 1 : 0);
+                refreshSettings();
+                return true;
+            } else if (preference == mTextColor) {
+                hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+                intHex = ColorPickerPreference.convertToColorInt(hex);
+                Settings.System.putInt(mResolver,
+                        Settings.System.LOCK_SCREEN_WEATHER_TEXT_COLOR, intHex);
+                preference.setSummary(hex);
+                return true;
+            } else if (preference == mIconColor) {
+                hex = ColorPickerPreference.convertToARGB(
+                Integer.valueOf(String.valueOf(newValue)));
+                intHex = ColorPickerPreference.convertToColorInt(hex);
+                Settings.System.putInt(mResolver,
+                        Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR, intHex);
+                preference.setSummary(hex);
+                return true;
             }
             return false;
+        }
+
+        private void showDialogInner(int id) {
+            DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+            newFragment.setTargetFragment(this, 0);
+            newFragment.show(getFragmentManager(), "dialog " + id);
+        }
+
+        public static class MyAlertDialogFragment extends DialogFragment {
+
+            public static MyAlertDialogFragment newInstance(int id) {
+                MyAlertDialogFragment frag = new MyAlertDialogFragment();
+                Bundle args = new Bundle();
+                args.putInt("id", id);
+                frag.setArguments(args);
+                return frag;
+            }
+
+            LockScreenSettingsFragment.SettingsPreferenceFragment getOwner() {
+                return (LockScreenSettingsFragment.SettingsPreferenceFragment) getTargetFragment();
+            }
+
+            @Override
+            public Dialog onCreateDialog(Bundle savedInstanceState) {
+                int id = getArguments().getInt("id");
+                switch (id) {
+                    case DLG_RESET:
+                        return new AlertDialog.Builder(getActivity())
+                        .setTitle(R.string.reset)
+                        .setMessage(R.string.dlg_reset_values_message)
+                        .setNegativeButton(R.string.cancel, null)
+                        .setNeutralButton(R.string.dlg_reset_android,
+                            new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Settings.System.putInt(getOwner().mResolver,
+                                        Settings.System.LOCK_SCREEN_SHOW_WEATHER, 0);
+                                Settings.System.putInt(getOwner().mResolver,
+                                        Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION, 1);
+                                Settings.System.putInt(getOwner().mResolver,
+                                        Settings.System.LOCK_SCREEN_SHOW_WEATHER_TIMESTAMP, 1);
+                                Settings.System.putInt(getOwner().mResolver,
+                                        Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON,
+                                        MONOCHROME_ICON);
+                                Settings.System.putInt(getOwner().mResolver,
+                                        Settings.System.LOCK_SCREEN_WEATHER_COLORIZE_ALL_ICONS, 0);
+                                Settings.System.putInt(getOwner().mResolver,
+                                        Settings.System.LOCK_SCREEN_WEATHER_TEXT_COLOR,
+                                        DEFAULT_COLOR);
+                                Settings.System.putInt(getOwner().mResolver,
+                                        Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR,
+                                        DEFAULT_COLOR);
+                                getOwner().refreshSettings();
+                            }
+                        })
+                        .create();
+                }
+                throw new IllegalArgumentException("unknown id " + id);
+            }
+
+            @Override
+            public void onCancel(DialogInterface dialog) {
+
+            }
         }
 
         @Override

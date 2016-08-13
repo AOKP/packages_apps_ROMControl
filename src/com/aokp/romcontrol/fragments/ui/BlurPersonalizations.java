6517edda832ba36b23dd1361722003adbcd4faec
LorDClockaN <lordclockan@gmail.com>
2016-08-13 17:07:27 +0200

    ROMControl - Blur: Main switch now restores previous switch values
    Change-Id: I708e7edac1206eff423d301391c7303390e0ad85

--- src/com/aokp/romcontrol/fragments/ui/BlurPersonalizations.java
+++ src/com/aokp/romcontrol/fragments/ui/BlurPersonalizations.java
@@ -24,6 +24,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.preference.ListPreference;
@@ -67,6 +69,12 @@ public class BlurPersonalizations extends Fragment {
 
         private static final String TAG = "BlurUI";
 
+        private SharedPreferences mBlurUISettings;
+        private Editor toEditBlurUISettings;
+        private String sNotifications = "false";
+        private String sHeader = "false";
+        private String sQuickSett = "false";
+
         //Switch Preferences
         private SwitchPreference mExpand;
         private SwitchPreference mNotiTrans;
@@ -90,6 +98,8 @@ public class BlurPersonalizations extends Fragment {
 
             PreferenceScreen prefSet = getPreferenceScreen();
             ContentResolver resolver = getActivity().getContentResolver();
+
+            mBlurUISettings = getActivity().getSharedPreferences("BlurUI", Context.MODE_PRIVATE);
 
             mExpand = (SwitchPreference) prefSet.findPreference("blurred_status_bar_expanded_enabled_pref");
             mExpand.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
@@ -173,20 +183,78 @@ public class BlurPersonalizations extends Fragment {
                 boolean enabled = ((SwitchPreference)preference).isChecked();
                 Settings.System.putInt(resolver,
                         Settings.System.STATUS_BAR_EXPANDED_ENABLED_PREFERENCE_KEY, enabled ? 1:0);
+                updatePrefs();
             } else if (preference == mNotiTrans) {
                 boolean enabled = ((SwitchPreference)preference).isChecked();
                 Settings.System.putInt(resolver,
                         Settings.System.TRANSLUCENT_NOTIFICATIONS_PREFERENCE_KEY, enabled ? 1:0);
+                if (enabled) {
+                    sNotifications = "true";
+                } else {
+                    sNotifications = "false";
+                }
+                toEditBlurUISettings = mBlurUISettings.edit();
+                toEditBlurUISettings.putString("notifications_transluency", sNotifications);
+                toEditBlurUISettings.commit();
             } else if (preference == mHeadSett) {
                 boolean enabled = ((SwitchPreference)preference).isChecked();
                 Settings.System.putInt(resolver,
                         Settings.System.TRANSLUCENT_HEADER_PREFERENCE_KEY, enabled ? 1:0);
+                if (enabled) {
+                    sHeader = "true";
+                } else {
+                    sHeader = "false";
+                }
+                toEditBlurUISettings = mBlurUISettings.edit();
+                toEditBlurUISettings.putString("header_transluency", sHeader);
+                toEditBlurUISettings.commit();
             } else if (preference == mQuickSett) {
                 boolean enabled = ((SwitchPreference)preference).isChecked();
                 Settings.System.putInt(resolver,
                         Settings.System.TRANSLUCENT_QUICK_SETTINGS_PREFERENCE_KEY, enabled ? 1:0);
+                if (enabled) {
+                    sQuickSett = "true";
+                } else {
+                    sQuickSett = "false";
+                }
+                toEditBlurUISettings = mBlurUISettings.edit();
+                toEditBlurUISettings.putString("quick_settings_transluency", sQuickSett);
+                toEditBlurUISettings.commit();
             }
             return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        public void sharedPreferences() {
+            toEditBlurUISettings = mBlurUISettings.edit();
+            toEditBlurUISettings.putString("notifications_transluency", sNotifications);
+            toEditBlurUISettings.putString("header_transluency", sHeader);
+            toEditBlurUISettings.putString("quick_settings_transluency", sQuickSett);
+            toEditBlurUISettings.commit();
+        }
+
+        private void updatePrefs() {
+            final ContentResolver resolver = getActivity().getContentResolver();
+
+            boolean tempNotification = mBlurUISettings.getString("notifications_transluency", "").equals("true");
+            boolean tempHeader = mBlurUISettings.getString("header_transluency", "").equals("true");
+            boolean tempQuickSett = mBlurUISettings.getString("quick_settings_transluency", "").equals("true");
+
+            if (Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_EXPANDED_ENABLED_PREFERENCE_KEY, 0) == 1) {
+                Settings.System.putInt(resolver,
+                        Settings.System.TRANSLUCENT_NOTIFICATIONS_PREFERENCE_KEY, tempNotification ? 1:0);
+                Settings.System.putInt(resolver,
+                        Settings.System.TRANSLUCENT_HEADER_PREFERENCE_KEY, tempHeader ? 1:0);
+                Settings.System.putInt(resolver,
+                        Settings.System.TRANSLUCENT_QUICK_SETTINGS_PREFERENCE_KEY, tempQuickSett ? 1:0);
+            } else {
+                Settings.System.putInt(resolver,
+                        Settings.System.TRANSLUCENT_NOTIFICATIONS_PREFERENCE_KEY, 0);
+                Settings.System.putInt(resolver,
+                        Settings.System.TRANSLUCENT_HEADER_PREFERENCE_KEY, 0);
+                Settings.System.putInt(resolver,
+                        Settings.System.TRANSLUCENT_QUICK_SETTINGS_PREFERENCE_KEY, 0);
+            }
         }
     }
 }

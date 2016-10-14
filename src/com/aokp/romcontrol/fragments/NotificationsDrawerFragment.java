19af9344e92f9f8191f408b3b34f7d453a61483a
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-10-14 14:29:24 +0200

    ROMControl - Custom QS rows/columns [2/2]
    *DU Edits*
    - Cleaned up some of the redundancy
    
    Change-Id: I05380b5ce5edb55775f559e1b4bd40a73ded506f

--- src/com/aokp/romcontrol/fragments/NotificationsDrawerFragment.java
+++ src/com/aokp/romcontrol/fragments/NotificationsDrawerFragment.java
@@ -67,8 +67,14 @@ public class NotificationsDrawerFragment extends Fragment {
         }
 
         private static final String PREF_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
+        private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
+        private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
+        private static final String PREF_COLUMNS = "qs_columns";
 
         private ListPreference mSysuiQqsCount;
+        private ListPreference mRowsPortrait;
+        private ListPreference mRowsLandscape;
+        private ListPreference mQsColumns;
 
         private ContentResolver mResolver;
 
@@ -83,6 +89,7 @@ public class NotificationsDrawerFragment extends Fragment {
             addPreferencesFromResource(R.xml.fragment_notificationsdrawer_settings);
             mResolver = getActivity().getContentResolver();
             PreferenceScreen prefSet = getPreferenceScreen();
+            int defaultValue;
 
             mSysuiQqsCount = (ListPreference) findPreference(PREF_SYSUI_QQS_COUNT);
             int SysuiQqsCount = Settings.Secure.getInt(mResolver,
@@ -90,6 +97,28 @@ public class NotificationsDrawerFragment extends Fragment {
             mSysuiQqsCount.setValue(Integer.toString(SysuiQqsCount));
             mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntry());
             mSysuiQqsCount.setOnPreferenceChangeListener(this);
+
+            mRowsPortrait = (ListPreference) findPreference(PREF_ROWS_PORTRAIT);
+            int rowsPortrait = Settings.Secure.getInt(mResolver,
+                    Settings.Secure.QS_ROWS_PORTRAIT, 3);
+            mRowsPortrait.setValue(String.valueOf(rowsPortrait));
+            mRowsPortrait.setSummary(mRowsPortrait.getEntry());
+            mRowsPortrait.setOnPreferenceChangeListener(this);
+
+            defaultValue = getResources().getInteger(com.android.internal.R.integer.config_qs_num_rows_landscape_default);
+            mRowsLandscape = (ListPreference) findPreference(PREF_ROWS_LANDSCAPE);
+            int rowsLandscape = Settings.Secure.getInt(mResolver,
+                    Settings.Secure.QS_ROWS_LANDSCAPE, defaultValue);
+            mRowsLandscape.setValue(String.valueOf(rowsLandscape));
+            mRowsLandscape.setSummary(mRowsLandscape.getEntry());
+            mRowsLandscape.setOnPreferenceChangeListener(this);
+
+            mQsColumns = (ListPreference) findPreference(PREF_COLUMNS);
+            int columnsQs = Settings.Secure.getInt(mResolver,
+                    Settings.Secure.QS_COLUMNS, 3);
+            mQsColumns.setValue(String.valueOf(columnsQs));
+            mQsColumns.setSummary(mQsColumns.getEntry());
+            mQsColumns.setOnPreferenceChangeListener(this);
 
             setHasOptionsMenu(true);
             return prefSet;
@@ -103,6 +132,8 @@ public class NotificationsDrawerFragment extends Fragment {
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
             mResolver = getActivity().getContentResolver();
+            int intValue;
+            int index;
 
             if (preference == mSysuiQqsCount) {
                 String SysuiQqsCount = (String) newValue;
@@ -111,6 +142,27 @@ public class NotificationsDrawerFragment extends Fragment {
                         Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
                 int SysuiQqsCountIndex = mSysuiQqsCount.findIndexOfValue(SysuiQqsCount);
                 mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
+                return true;
+            } else if (preference == mRowsPortrait) {
+                intValue = Integer.valueOf((String) newValue);
+                index = mRowsPortrait.findIndexOfValue((String) newValue);
+                Settings.Secure.putInt(mResolver,
+                        Settings.Secure.QS_ROWS_PORTRAIT, intValue);
+                preference.setSummary(mRowsPortrait.getEntries()[index]);
+                return true;
+            } else if (preference == mRowsLandscape) {
+                intValue = Integer.valueOf((String) newValue);
+                index = mRowsLandscape.findIndexOfValue((String) newValue);
+                Settings.Secure.putInt(mResolver,
+                        Settings.Secure.QS_ROWS_LANDSCAPE, intValue);
+                preference.setSummary(mRowsLandscape.getEntries()[index]);
+                return true;
+            } else if (preference == mQsColumns) {
+                intValue = Integer.valueOf((String) newValue);
+                index = mQsColumns.findIndexOfValue((String) newValue);
+                Settings.Secure.putInt(mResolver,
+                        Settings.Secure.QS_COLUMNS, intValue);
+                preference.setSummary(mQsColumns.getEntries()[index]);
                 return true;
             }
             return false;

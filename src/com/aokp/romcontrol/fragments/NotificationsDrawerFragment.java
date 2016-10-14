e95864b630220bd07f79a4aa30e0bbf766f0298e
xyyx <xyyx@mail.ru>
2016-10-14 14:31:58 +0200

    ROMControl - Custom small QS tiles [2/2]
    Change-Id: I42d7df7a3f8a3ddc562fdf32caf27402c1b5aba5
    
    Signed-off-by: STELIX <ssspinni@gmail.com>

---
+++ src/com/aokp/romcontrol/fragments/NotificationsDrawerFragment.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2016 The Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aokp.romcontrol.fragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.aokp.romcontrol.R;
+
+public class NotificationsDrawerFragment extends Fragment {
+
+    public NotificationsDrawerFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().getFragmentManager().beginTransaction()
+                .replace(R.id.container, new NotificationsDrawerSettingsPreferenceFragment())
+                .commit();
+    }
+
+    public static class NotificationsDrawerSettingsPreferenceFragment extends PreferenceFragment
+            implements OnPreferenceChangeListener {
+
+        public NotificationsDrawerSettingsPreferenceFragment() {
+
+        }
+
+        private static final String PREF_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
+
+        private ListPreference mSysuiQqsCount;
+
+        private ContentResolver mResolver;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            createCustomView();
+        }
+
+        private PreferenceScreen createCustomView() {
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.fragment_notificationsdrawer_settings);
+            mResolver = getActivity().getContentResolver();
+            PreferenceScreen prefSet = getPreferenceScreen();
+
+            mSysuiQqsCount = (ListPreference) findPreference(PREF_SYSUI_QQS_COUNT);
+            int SysuiQqsCount = Settings.Secure.getInt(mResolver,
+                   Settings.Secure.QQS_COUNT, 5);
+            mSysuiQqsCount.setValue(Integer.toString(SysuiQqsCount));
+            mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntry());
+            mSysuiQqsCount.setOnPreferenceChangeListener(this);
+
+            setHasOptionsMenu(true);
+            return prefSet;
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            mResolver = getActivity().getContentResolver();
+
+            if (preference == mSysuiQqsCount) {
+                String SysuiQqsCount = (String) newValue;
+                int SysuiQqsCountValue = Integer.parseInt(SysuiQqsCount);
+                Settings.Secure.putInt(mResolver,
+                        Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
+                int SysuiQqsCountIndex = mSysuiQqsCount.findIndexOfValue(SysuiQqsCount);
+                mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
+                return true;
+            }
+            return false;
+        }
+    }
+}

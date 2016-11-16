463ccdcbf125fe0ac6500981434c39b0735a96f8
Alex Avance <aravance@gmail.com>
2016-11-16 15:48:42 +0100

    ROMControl - Add an option to change the device hostname
    This adds an option to modify the device hostname used
    in ip resolution. This is useful when connecting to the
    android device in a dynamic dhcp environment.
    
    Change-Id: I592e9a9ebcc45b24d0069f16e3738378faf321f6
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- src/com/aokp/romcontrol/MainActivity.java
+++ src/com/aokp/romcontrol/MainActivity.java
@@ -30,13 +30,15 @@ import android.view.MenuItem;
 
 import android.view.View;
 
+import com.aokp.romcontrol.fragments.NavigationDrawerFragment;
+
 import com.aokp.romcontrol.fragments.about.AboutCrewFragment;
 import com.aokp.romcontrol.fragments.about.AboutFragment;
 import com.aokp.romcontrol.fragments.about.AboutTabHostFragment;
 
 import com.aokp.romcontrol.fragments.general.GeneralTabHostFragment;
 import com.aokp.romcontrol.fragments.general.GeneralSettingsFragment;
-import com.aokp.romcontrol.fragments.NavigationDrawerFragment;
+import com.aokp.romcontrol.fragments.general.HostnamePreference;
 
 import com.aokp.romcontrol.fragments.sound.SoundTabHostFragment;
 import com.aokp.romcontrol.fragments.sound.SoundSettingsFragment;

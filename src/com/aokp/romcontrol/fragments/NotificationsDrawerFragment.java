a570e892440d30bf477d137b83ec9d856a18ad3e
maxwen <max.weninger@gmail.com>
2018-01-22 06:15:00 +0300

    ROMControl - custom header image provider [2/2]
    reads image uri and saves it in the file custom_file_header_image
    in /data/user_de/0/com.android.systemui/files/file_header_image
    must be copied else loading on reboot will fail cause storage
    may not be mounted
    
    Change-Id: I1304f686102924275fa6ecdbc369e55df82987c3

--- src/com/aokp/romcontrol/fragments/NotificationsDrawerFragment.java
+++ src/com/aokp/romcontrol/fragments/NotificationsDrawerFragment.java
@@ -25,6 +25,7 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.preference.ListPreference;
@@ -82,6 +83,9 @@ public class NotificationsDrawerFragment extends Fragment {
         private static final String CUSTOM_HEADER_PROVIDER = "custom_header_provider";
         private static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
         private static final String CUSTOM_HEADER_ENABLED = "status_bar_custom_header";
+        private static final String FILE_HEADER_SELECT = "file_header_select";
+
+        private static final int REQUEST_PICK_IMAGE = 0;
 
         private ListPreference mSysuiQqsCount;
         private ListPreference mRowsPortrait;
@@ -94,6 +98,8 @@ public class NotificationsDrawerFragment extends Fragment {
         private ListPreference mHeaderProvider;
         private String mDaylightHeaderProvider;
         private SwitchPreference mHeaderEnabled;
+        private Preference mFileHeader;
+        private String mFileHeaderProvider;
 
         private ContentResolver mResolver;
 
@@ -140,7 +146,6 @@ public class NotificationsDrawerFragment extends Fragment {
             mQsColumns.setOnPreferenceChangeListener(this);
 
             mHeaderBrowse = findPreference(CUSTOM_HEADER_BROWSE);
-            mHeaderBrowse.setEnabled(isBrowseHeaderAvailable());
 
             mHeaderEnabled = (SwitchPreference) findPreference(CUSTOM_HEADER_ENABLED);
             mHeaderEnabled.setOnPreferenceChangeListener(this);
@@ -165,24 +170,45 @@ public class NotificationsDrawerFragment extends Fragment {
             mHeaderShadow.setOnPreferenceChangeListener(this);
 
             mDaylightHeaderProvider = getResources().getString(R.string.daylight_header_provider);
+            mFileHeaderProvider = getResources().getString(R.string.file_header_provider);
             String providerName = Settings.System.getString(resolver,
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_PROVIDER);
             if (providerName == null) {
                 providerName = mDaylightHeaderProvider;
             }
+            mHeaderBrowse.setEnabled(isBrowseHeaderAvailable() && !providerName.equals(mFileHeaderProvider));
             mHeaderProvider = (ListPreference) findPreference(CUSTOM_HEADER_PROVIDER);
             int valueIndex = mHeaderProvider.findIndexOfValue(providerName);
             mHeaderProvider.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
             mHeaderProvider.setSummary(mHeaderProvider.getEntry());
             mHeaderProvider.setOnPreferenceChangeListener(this);
             mDaylightHeaderPack.setEnabled(providerName.equals(mDaylightHeaderProvider));
+            mFileHeader = findPreference(FILE_HEADER_SELECT);
+            mFileHeader.setEnabled(providerName.equals(mFileHeaderProvider));
 
             setHasOptionsMenu(true);
             return prefSet;
         }
 
         @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent result) {
+            if (requestCode == REQUEST_PICK_IMAGE) {
+                if (resultCode != Activity.RESULT_OK) {
+                return;
+                }
+                final Uri imageUri = result.getData();
+                Settings.System.putString(getContentResolver(), Settings.System.STATUS_BAR_FILE_HEADER_IMAGE, imageUri.toString());
+            }
+        }
+
+        @Override
         public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            if (preference == mFileHeader) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_PICK_IMAGE);
+                return true;
+            }
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
 
@@ -242,8 +268,10 @@ public class NotificationsDrawerFragment extends Fragment {
                 int valueIndex = mHeaderProvider.findIndexOfValue(value);
                 mHeaderProvider.setSummary(mHeaderProvider.getEntries()[valueIndex]);
                 mDaylightHeaderPack.setEnabled(value.equals(mDaylightHeaderProvider));
+                mHeaderBrowse.setEnabled(!value.equals(mFileHeaderProvider));
                 mHeaderBrowse.setTitle(valueIndex == 0 ? R.string.custom_header_browse_title : R.string.custom_header_pick_title);
                 mHeaderBrowse.setSummary(valueIndex == 0 ? R.string.custom_header_browse_summary_new : R.string.custom_header_pick_summary);
+                mFileHeader.setEnabled(value.equals(mFileHeaderProvider));
                 return true;
             } else if (preference == mHeaderEnabled) {
                 Boolean headerEnabled = (Boolean) newValue;

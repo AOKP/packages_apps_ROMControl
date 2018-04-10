3cb1b7a75c0e74e6b0b146cfa0060459f7bc0c95
villarleg <santialovera20@gmail.com>
2018-04-10 15:21:54 +0200

    ROMControl: Implement Battery Bar from the following commits
    BatteryBar [2/2] by cphelps76
    Battery Bar Enhancements [2/2] by SpiritCroc
    
    Thanks to AICP and RR Team
    
    Change-Id: I575d97d504c7eef4161c9a1de6e5ff48ac58b895

--- src/com/aokp/romcontrol/MainActivity.java
+++ src/com/aokp/romcontrol/MainActivity.java
@@ -53,6 +53,7 @@ import com.aokp.romcontrol.fragments.sound.LiveVolumeFragment;
 import com.aokp.romcontrol.fragments.statusbar.StatusBarTabHostFragment;
 import com.aokp.romcontrol.fragments.statusbar.StatusbarSettingsFragment;
 import com.aokp.romcontrol.fragments.statusbar.TrafficSettingsFragment;
+import com.aokp.romcontrol.fragments.statusbar.BatteryBarFragment;
 
 import com.aokp.romcontrol.fragments.ui.UITabHostFragment;
 import com.aokp.romcontrol.fragments.ui.AnimBarPreference;

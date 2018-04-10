a80b30b568829a5228440f0826dd1c33da41b015
cphelps76 <cphelps76@droidconcepts.com>
2018-04-10 15:21:54 +0200

    ROMControl: [SQUASH]BatteryBar [2/2]
    BatteryBar [2/2] by cphelps76
    Battery Bar Enhancements [2/2] by SpiritCroc
    
    Thanks to AIMROM
    
    Change-Id: I575d97d504c7eef4161c9a1de6e5ff48ac58b895

--- Android.mk
+++ Android.mk
@@ -7,15 +7,28 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.cfg
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v14-preference \
     android-support-v13 \
+    android-support-v7-recyclerview \
+    android-support-v7-preference \
+    android-support-v7-cardview \
+    android-support-v7-appcompat \
     android-support-v4 \
+    android-support-design \
     AndroidAsync \
     gson \
     jsr305 \
     org.lineageos.platform.internal
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files, src)
-LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR += $(SUPPORT_LIBRARY_ROOT)/v14/preference/res
+LOCAL_RESOURCE_DIR += $(SUPPORT_LIBRARY_ROOT)/v7/appcompat/res
+LOCAL_RESOURCE_DIR += $(SUPPORT_LIBRARY_ROOT)/v7/cardview/res
+LOCAL_RESOURCE_DIR += $(SUPPORT_LIBRARY_ROOT)/v7/preference/res
+LOCAL_RESOURCE_DIR += $(SUPPORT_LIBRARY_ROOT)/v7/recyclerview/res
+LOCAL_RESOURCE_DIR += $(SUPPORT_LIBRARY_ROOT)/design/res
 
 LOCAL_PACKAGE_NAME := ROMControl
 LOCAL_CERTIFICATE := platform
